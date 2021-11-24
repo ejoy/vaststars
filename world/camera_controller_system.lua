4871b448c68f4bec89ec6a6cabb8e130b389fde2
kinbei <kinbei@qq.com>
2021-11-24 14:03:51 +0800

    camera controller system

---
+++ world/camera_controller_system.lua
@@ -0,0 +1,117 @@
+local ecs = ...
+local world = ecs.world
+local w = world.w
+
+local rhwi      = import_package "ant.hwi"
+
+local icamera = ecs.import.interface "ant.camera|icamera"
+
+local icc = ecs.interface "icamera_controller"
+
+local controller = {}
+function icc.attach(camera_ref)
+	controller.camera_ref = camera_ref
+end
+
+function icc.camera()
+	return controller.camera_ref
+end
+
+local cc_sys = ecs.system "camera_controller_system"
+
+local iom = ecs.import.interface "ant.objcontroller|iobj_motion"
+local mouse_events = {
+	world:sub {"mouse", "RIGHT"}
+}
+
+local kMouseSpeed <const> = 0.5
+local mouse_lastx, mouse_lasty
+local dpi_x, dpi_y
+
+local eventKeyboard = world:sub {"keyboard"}
+local kKeyboardSpeed <const> = 0.5
+
+function cc_sys:post_init()
+	dpi_x, dpi_y = rhwi.dpi()
+end
+
+local function can_rotate(camera)
+	local lt = camera.lock_target
+	return lt and lt.type ~= "rotate" or true
+end
+
+local function can_move(camera)
+	local lock_target = camera.lock_target
+	return lock_target and lock_target.type ~= "move" or true
+end
+
+local mw_mb = world:sub{"mouse_wheel"}
+
+local function can_orthoview_scale(camera_ref)
+	local f = icamera.get_frustum(camera_ref)
+	return f.ortho
+end
+
+local function scale_orthoview(camera_ref, delta)
+	local scale_speed = 0.25
+	local f = icamera.get_frustum(camera_ref)
+	local d = delta * scale_speed
+	f.l = f.l - d
+	f.r = f.r + d
+	f.b = f.b - d
+	f.t = f.t + d
+
+	icamera.set_frustum(camera_ref, f)
+end
+
+function cc_sys:init_world()
+	for e in w:select "main_queue camera_ref:in" do
+        icc.attach(e.camera_ref)
+    end
+end
+
+function cc_sys:data_changed()
+	local camera_ref = icc.camera()
+
+	if can_rotate(camera_ref) then
+		for _, e in ipairs(mouse_events) do
+			for _,_,state,x,y in e:unpack() do
+				if state == "MOVE" and mouse_lastx then
+					local ux = (x - mouse_lastx) / dpi_x * kMouseSpeed
+					local uy = (y - mouse_lasty) / dpi_y * kMouseSpeed
+					iom.rotate_forward_vector(camera_ref, uy, ux)
+				end
+				mouse_lastx, mouse_lasty = x, y
+			end
+		end
+	end
+
+	if can_orthoview_scale(camera_ref) then
+		for _, delta in mw_mb:unpack() do
+			scale_orthoview(camera_ref, delta)
+		end
+	end
+
+	if can_move(camera_ref) then
+		local keyboard_delta = {0 , 0, 0}
+		for _,code,press in eventKeyboard:unpack() do
+			local delta = (press>0) and kKeyboardSpeed or 0
+			if code == "A" then
+				keyboard_delta[1] = keyboard_delta[1] - delta
+			elseif code == "D" then
+				keyboard_delta[1] = keyboard_delta[1] + delta
+			elseif code == "Q" then
+				keyboard_delta[2] = keyboard_delta[2] - delta
+			elseif code == "E" then
+				keyboard_delta[2] = keyboard_delta[2] + delta
+			elseif code == "W" then
+				keyboard_delta[3] = keyboard_delta[3] + delta
+			elseif code == "S" then
+				keyboard_delta[3] = keyboard_delta[3] - delta
+			end
+		end
+		if keyboard_delta[1] ~= 0 or keyboard_delta[2] ~= 0 or keyboard_delta[3] ~= 0 then
+			iom.move(camera_ref, keyboard_delta)
+		end
+	end
+end
--- world/init_system.lua
+++ world/init_system.lua
@@ -1,14 +1,25 @@
 local ecs = ...
 local world = ecs.world
-local m = ecs.system 'init_system'
-local irq = ecs.import.interface "ant.render|irenderqueue"
+local w = world.w
 
+local mathpkg = import_package "ant.math"
+local irq = ecs.import.interface "ant.render|irenderqueue"
+local iom = ecs.import.interface "ant.objcontroller|iobj_motion"
+local iterrain = ecs.import.interface "vaststars|iterrain"
+
+local math3d = require "math3d"
+local mc = mathpkg.constant
+
+local m = ecs.system 'init_system'
 function m:init_world()
-    irq.set_view_clear_color("main_queue", 0xff0000ff)
+    local mq = w:singleton("main_queue", "camera_ref:in")
+    local eyepos = math3d.vector(0, 8, -8)
+    local camera_ref = mq.camera_ref
+    iom.set_position(camera_ref, eyepos)
+    local dir = math3d.normalize(math3d.sub(mc.ZERO_PT, eyepos))
+    iom.set_direction(camera_ref, dir)
+
     ecs.create_instance "/res/light_directional.prefab"
     ecs.create_instance "/res/skybox.prefab"
-
-    local camera = ecs.require "camera"
-    world:call(camera.root, "set_position", {1, 1, 1})
-    camera:send "hello"
+    iterrain.create()
 end
