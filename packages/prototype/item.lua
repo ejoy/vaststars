13fe26e6d723498bfe6e0cfb5b0c72d0217a749b
uglyfrog78 <rendong.ren@alibaba-inc.com>
2021-12-20 14:13:12 +0800

    修改item配置

--- packages/prototype/item.lua
+++ packages/prototype/item.lua
@@ -2,26 +2,23 @@ local gameplay = import_package "vaststars.gameplay"
 local prototype = gameplay.prototype
 
 prototype "iron ignot" {
-    type = { "recipe" },
-    ingredients = {
-        {"iron ore", 5},
-    },
-    results = {
-        {"iron ignot", 2},
-        {"gravel", 1}
-    },
-    time = "8s"
+    type = {"item"},
+    stack = 100,
 }
 
 prototype "iron plate" {
-    type = { "recipe" },
-    ingredients = {
-        {"iron ignot", 4},
-    },
-    results = {
-        {"iron plate", 3}
-    },
-    time = "3s"
+    type = {"item"},
+    stack = 100,
+}
+
+prototype "iron ore" {
+    type = {"item"},
+    stack = 100,
+}
+
+prototype "gravel" {
+    type = {"item"},
+    stack = 100,
 }
 
 prototype "copper plate" {
