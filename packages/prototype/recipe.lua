5a91d43787fa6b1b435b6ea0830744768c37c9d5
uglyfrog78 <rendong.ren@alibaba-inc.com>
2021-12-21 15:51:55 +0800

    更新配方中文

--- packages/prototype/recipe.lua
+++ packages/prototype/recipe.lua
@@ -3,13 +3,13 @@ local prototype = gameplay.prototype
 
 prototype "铁锭" {
     type = { "recipe" },
-    category = "smelting",
+    category = "金属冶炼",
     ingredients = {
-        {"iron ore", 5},
+        {"铁矿石", 5},
     },
     results = {
-        {"iron ignot", 2},
-        {"gravel", 1}
+        {"铁锭", 2},
+        {"碎石", 1}
     },
     time = "8s",
     description = "铁矿石通过金属冶炼获得铁锭",
@@ -17,12 +17,12 @@ prototype "铁锭" {
 
 prototype "铁板1" {
     type = { "recipe" },
-    category = "machine-casting",
+    category = "金属锻造",
     ingredients = {
-        {"iron ignot", 4},
+        {"铁锭", 4},
     },
     results = {
-        {"iron plate", 3}
+        {"铁板", 3}
     },
     time = "3s",
     description = "使用铁锭锻造铁板",
@@ -31,13 +31,13 @@ prototype "铁板1" {
 
 prototype "铁板2" {
     type = { "recipe" },
-    category = "machine-casting",
+    category = "金属锻造",
     ingredients = {
-        {"iron ignot", 4},
-        {"gravel", 2}
+        {"铁锭", 4},
+        {"碎石", 2}
     },
     results = {
-        {"iron plate", 5}
+        {"铁板", 5}
     },
     time = "5s",
     description = "使用铁锭和碎石锻造铁板",
