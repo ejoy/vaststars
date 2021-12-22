a99f150702fdf1016dee67004e02dbb99cee4e5a
uglyfrog78 <rendong.ren@alibaba-inc.com>
2021-12-22 16:59:58 +0800

    更新气体配方

--- packages/prototype/fluid.lua
+++ packages/prototype/fluid.lua
@@ -4,7 +4,22 @@ local prototype = gameplay.prototype
 prototype "空气" {
     type = {"fluid"},
     heat_capacity = "0.08KJ",
-    fuel_value = "32KJ",
+    default_temperature = 50,
+    max_temperature = 100,
+    des = "大气层中的基本气体",
+}
+
+prototype "氮气" {
+    type = {"fluid"},
+    heat_capacity = "0.08KJ",
+    default_temperature = 50,
+    max_temperature = 100,
+    des = "大气层中的基本气体",
+}
+
+prototype "二氧化碳" {
+    type = {"fluid"},
+    heat_capacity = "0.08KJ",
     default_temperature = 50,
     max_temperature = 100,
     des = "大气层中的基本气体",
--- packages/prototype/recipe.lua
+++ packages/prototype/recipe.lua
@@ -633,3 +633,31 @@ prototype "机械科技包" {
     time = "15s",
     description = "使用铁锭和碎石锻造铁板",
 }
+
+prototype "机械科技包" {
+    type = { "recipe" },
+    category = "中型制造",
+    ingredients = {
+        {"电动机1", 1},
+        {"铁齿轮", 3},
+    },
+    results = {
+        {"机械科技包", 1},
+    },
+    time = "15s",
+    description = "使用铁锭和碎石锻造铁板",
+}
+
+prototype "空气分离1" {
+    type = { "recipe" },
+    category = "过滤",
+    ingredients = {
+        {"空气", 150},
+    },
+    results = {
+        {"氮气", 90},
+        {"二氧化碳", 40},
+    },
+    time = "1s",
+    description = "使用铁锭和碎石锻造铁板",
+}
