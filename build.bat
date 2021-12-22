939e5319bfbf7d96371f03bb8ba0ce0d9e532ff5
kinbei <kinbei@qq.com>
2021-12-23 07:56:53 +0800

    batch script

--- build.bat
+++ build.bat
@@ -6,9 +6,4 @@ pushd %CURRENT_DIR%
 %CURRENT_DIR%\..\luamake\luamake.exe tools -mode debug
 popd
 
-pushd %CURRENT_DIR%\3rd\ant\
-%CURRENT_DIR%\..\luamake\luamake.exe -mode debug
-%CURRENT_DIR%\..\luamake\luamake.exe tools -mode debug
-popd
-
 pause
--- clean.bat
+++ clean.bat
@@ -3,6 +3,8 @@
 
 pushd %current_dir%
 rd /s /q .build
+rd /s /q build
+rd /s /q bin
 rd /s /q .\3rd\ant\tools\prefab_editor\.build
 popd
 
