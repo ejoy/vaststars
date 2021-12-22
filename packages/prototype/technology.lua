c9d962faa44e2b9b85bfdffe60d3f88e0ff71551
uglyfrog78 <rendong.ren@alibaba-inc.com>
2021-12-22 09:49:04 +0800

    更新科技配置表

--- packages/prototype/technology.lua
+++ packages/prototype/technology.lua
@@ -3,42 +3,17 @@ local prototype = gameplay.prototype
 
 
 prototype "地质瓶研究" {
-    effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = "地质瓶1"
-      },
+    effects = {
+      {"unlock-recipe","地质瓶1"},
+      {"unlock-recipe","铁锭"},
     },
     prerequisites = {"无"},
-    unit =
-    {
-      count = 5,
-      ingredients = 
-      {
-        {"无"},
-      },
-      time = 3
+    ingredients = {
+        {"地质瓶", 1},
+        {"空气瓶", 1},
     },
+    time = 4,
 }
 
 
-prototype "铁矿石熔炼" {
-    effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = "铁锭"
-      },
-    },
-    prerequisites = {"地质瓶研究"},
-    unit =
-    {
-      count = 4,
-      ingredients = 
-      {
-        {"地质瓶"},
-      },
-      time = 4
-    },
-}
+
