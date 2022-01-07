840a8355e2f452b1b37ef10ab8410e86dc7c45da
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-01-07 16:25:24 +0800

    update assembler

---
+++ packages/prototype/entity/assembler.lua
@@ -0,0 +1,32 @@
+local gameplay = import_package "vaststars.gameplay"
+local prototype = gameplay.prototype
+
+prototype "组装机1" {
+    type = {"entity", "assembling", "consumer", "fluidboxes"},
+    area = "3x3",
+    speed = "100%",
+    power = "150kW",
+    priority = "secondary",
+    fluidboxes = {
+        input = {
+            {
+                capacity = 500,
+                height = 100,
+                base_level = -100,
+                connections = {
+                    {type="input", position={1,0,"N"}},
+                }
+            },
+        },
+        output = {
+            {
+                capacity = 500,
+                height = 100,
+                base_level = 100,
+                connections = {
+                    {type="output", position={1,2,"S"}},
+                }
+            },
+        },
+    }
+}
