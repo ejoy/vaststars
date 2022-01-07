e3fb893a8d5499ca596162bddc4f00d51efbaaa6
actboy168 <actboy168@gmail.com>
2022-01-07 15:55:39 +0800

    fixes bug

--- packages/gameplay/type/assembling.lua
+++ packages/gameplay/type/assembling.lua
@@ -11,43 +11,70 @@ local function isFluidId(id)
     return id & 0x0C00 == 0x0C00
 end
 
-local function getFluidList(s, max)
-    local r = {}
-    for i = 1, #s, 4 do
-        local id = string.unpack("<I2", s, i)
+local function createContainerAndFluidBox(fluidboxes, s, max)
+    assert(#s <= 4 * 15)
+    local container = {}
+    local fluids = {}
+    for idx = 1, #s//4 do
+        local id, n = string.unpack("<I2I2", s, 4*idx-3)
+        local limit = 0
         if isFluidId(id) then
-            r[#r+1] = (i-1)//4+1
+            fluids[#fluids+1] = idx
+            limit = fluidboxes[#fluids].capacity
+        else
+            limit = n * 2
         end
+        container[#container+1] = string.pack("<I2I2", id, limit)
     end
-    if #r > max then
+    container = table.concat(container)
+    if #fluids > max then
         error "The assembling does not support this recipe."
     end
-    for i = #r+1, max do
-        r[i] = 0
+    for i = #fluids+1, max do
+        fluids[i] = 0
     end
-    return r
-end
-
-local function getFluidBox(s, max)
-    assert(#s <= 15)
-    local fluids = getFluidList(s, max)
     local fb = 0
     for i = max, 1, -1 do
         fb = (fb << 4) | fluids[i]
     end
-    return fb
+    return container, fb
+end
+
+local function createContainer(s)
+    local container = {}
+    for idx = 1, #s//4 do
+        local id, n = string.unpack("<I2I2", s, 4*idx-3)
+        assert(not isFluidId(id))
+        local limit = n * 2
+        container[#container+1] = string.pack("<I2I2", id, limit)
+    end
+    return table.concat(container)
 end
 
 function c:ctor(init, pt)
-    --TODO fluid limit
     local recipe_name = pt.recipe and pt.recipe or init.recipe
     local recipe = assert(prototype.query("recipe", recipe_name), "unknown recipe: "..recipe_name)
+    if not pt.fluidboxes then
+        local container_in = createContainer(recipe.ingredients)
+        local container_out = createContainer(recipe.results)
+        return {
+            assembling = {
+                recipe = recipe.id,
+                container = self:container_create("assembling", container_in, container_out),
+                fluidbox_in = 0,
+                fluidbox_out = 0,
+                process = STATUS_IDLE,
+            }
+        }
+    end
+    local container_in, fluidbox_in = createContainerAndFluidBox(pt.fluidboxes.input, recipe.ingredients, 4)
+    local container_out, fluidbox_out = createContainerAndFluidBox(pt.fluidboxes.output, recipe.results, 3)
     return {
         assembling = {
             recipe = recipe.id,
-            container = self:container_create("assembling", recipe.ingredients, recipe.results),
-            fluidbox_in = getFluidBox(recipe.ingredients, 4),
-            fluidbox_out = getFluidBox(recipe.results, 3),
+            container = self:container_create("assembling", container_in, container_out),
+            fluidbox_in = fluidbox_in,
+            fluidbox_out = fluidbox_out,
             process = STATUS_IDLE,
         }
     }
