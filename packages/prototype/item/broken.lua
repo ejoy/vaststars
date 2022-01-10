0c3fe49537670815595b3c0f52f5a5ae83306269
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-01-10 09:57:16 +0800

    modify parameters

---
+++ packages/prototype/item/broken.lua
@@ -0,0 +1,68 @@
+local gameplay = import_package "vaststars.gameplay"
+local prototype = gameplay.prototype
+
+prototype "破损空气过滤器" {
+    type = {"item"},
+    stack = 10,
+    description = "处理海水的工厂",
+}
+
+prototype "破损水电站" {
+    type = {"item"},
+    stack = 10,
+    description = "处理海水的工厂",
+}
+
+prototype "破损电解厂" {
+    type = {"item"},
+    stack = 10,
+    description = "处理海水的工厂",
+}
+
+prototype "破损化工厂" {
+    type = {"item"},
+    stack = 10,
+    description = "处理海水的工厂",
+}
+
+prototype "破损组装机" {
+    type = {"item"},
+    stack = 10,
+    description = "处理海水的工厂",
+}
+
+prototype "破损铁制电线杆" {
+    type = {"item"},
+    stack = 50,
+    description = "处理海水的工厂",
+}
+
+prototype "破损太阳能板" {
+    type = {"item"},
+    stack = 50,
+    description = "处理海水的工厂",
+}
+
+prototype "破损蓄电池" {
+    type = {"item"},
+    stack = 50,
+    description = "处理海水的工厂",
+}
+
+prototype "破损物流中心" {
+    type = {"item"},
+    stack = 50,
+    description = "处理海水的工厂",
+}
+
+prototype "破损运输车辆" {
+    type = {"item"},
+    stack = 50,
+    description = "处理海水的工厂",
+}
+
+prototype "破损车站" {
+    type = {"item"},
+    stack = 50,
+    description = "处理海水的工厂",
+}
