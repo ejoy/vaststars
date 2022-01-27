f8c022ddb47ef2ab8192d138060d124f00f60ad7
kinbei <kinbei@qq.com>
2022-01-27 11:09:54 +0800

    ui config

--- packages/resources/ui/config/construct.lua
+++ packages/resources/ui/config/construct.lua
@@ -1,27 +1,47 @@
 local menu = {
-    [1] = {
+    {
         name = "水管",
         image = "construct/process.png",
+        detail = {
+            {
+                prototype = "组装机1",
+                image = "construct/process.png",
+            },
+            {
+                prototype = "组装机1",
+                image = "construct/process.png",
+            },
+        }
     },
-    [2] = {
+    {
         name = "矿石",
         image = "construct/chemical.png",
+        detail = {
+        },
     },
-    [3] = {
+    {
         name = "仓库",
         image = "construct/power.png",
+        detail = {
+        },
     },
-    [4] = {
+    {
         name = "物流中心",
         image = "construct/logistics_center.png",
+        detail = {
+        },
     },
-    [5] = {
+    {
         name = "车站",
         image = "construct/goods_station.png",
+        detail = {
+        },
     },
-    [6] = {
-        name = "车站",
+    {
+        name = "道路",
         image = "construct/road.png",
+        detail = {
+        },
     },
 }
 return menu
