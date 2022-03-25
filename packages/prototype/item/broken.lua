8b40636b007368278e398e6fd805ad0a0e687ac6
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-03-25 16:03:29 +0800

    update desc

--- packages/prototype/item/broken.lua
+++ packages/prototype/item/broken.lua
@@ -4,65 +4,65 @@ local prototype = gameplay.prototype
 prototype "破损空气过滤器" {
     type = {"item"},
     stack = 10,
-    description = "处理海水的工厂",
+    description = "损坏的空气过滤器",
 }
 
 prototype "破损水电站" {
     type = {"item"},
     stack = 10,
-    description = "处理海水的工厂",
+    description = "损坏的水电站",
 }
 
 prototype "破损电解厂" {
     type = {"item"},
     stack = 10,
-    description = "处理海水的工厂",
+    description = "损坏的电解厂",
 }
 
 prototype "破损化工厂" {
     type = {"item"},
     stack = 10,
-    description = "处理海水的工厂",
+    description = "损坏的化工厂",
 }
 
 prototype "破损组装机" {
     type = {"item"},
     stack = 10,
-    description = "处理海水的工厂",
+    description = "损坏的组装机",
 }
 
 prototype "破损铁制电线杆" {
     type = {"item"},
     stack = 50,
-    description = "处理海水的工厂",
+    description = "损坏的铁制电线杆",
 }
 
 prototype "破损太阳能板" {
     type = {"item"},
     stack = 50,
-    description = "处理海水的工厂",
+    description = "损坏的太阳能板",
 }
 
 prototype "破损蓄电池" {
     type = {"item"},
     stack = 50,
-    description = "处理海水的工厂",
+    description = "损坏的蓄电池",
 }
 
 prototype "破损物流中心" {
     type = {"item"},
     stack = 50,
-    description = "处理海水的工厂",
+    description = "损坏的物流中心",
 }
 
 prototype "破损运输车辆" {
     type = {"item"},
     stack = 50,
-    description = "处理海水的工厂",
+    description = "损坏的运输车辆",
 }
 
 prototype "破损车站" {
     type = {"item"},
     stack = 50,
-    description = "处理海水的工厂",
+    description = "损坏的车站",
 }
