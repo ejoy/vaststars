fbd99ca22884401b0a781ef4154839a211470eb8
kinbei <kinbei@qq.com>
2022-03-26 09:55:57 +0800

    support gesture tap

--- packages/gamerender/drapdrop_system.lua
+++ packages/gamerender/drapdrop_system.lua
@@ -4,6 +4,7 @@ local w = world.w
 
 local pickup_mapping_mb = world:sub {"pickup_mapping"}
 local mouse_mb = world:sub {"mouse"}
+local touch_mb = world:sub {"touch"}
 
 local drapdrop_system = ecs.system "drapdrop_system"
 
@@ -19,6 +20,19 @@ function drapdrop_system:data_changed()
                     e.drapdrop_selected = false
                     w:sync("drapdrop_selected:out", e)
                 end
+            end
+        end
+    end
+
+    for _, state, data in touch_mb:unpack() do
+        if state == "MOVE" then
+            for game_object in w:select "drapdrop_selected:in id:in" do
+                world:pub {"drapdrop_entity", game_object.id, data[1].x, data[1].y}
+            end
+        elseif state == "END" then
+            for e in w:select "drapdrop_selected:in" do
+                e.drapdrop_selected = false
+                w:sync("drapdrop_selected:out", e)
             end
         end
     end
