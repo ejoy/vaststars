58aefb14871858b9406b7fd24ac1dc1c039523d9
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-03-28 10:37:43 +0800

    update

--- packages/prototype/entity/powerpole.lua
+++ packages/prototype/entity/powerpole.lua
@@ -2,7 +2,7 @@ local gameplay = import_package "vaststars.gameplay"
 local prototype = gameplay.prototype
 
 prototype "铁制电线杆" {
-    model = "prefabs/assembling-1.prefab",
+    model = "prefabs/electric-pole-1.prefab",
     type = {"entity", "pole"},
     area = "1x1",
     supply_area = "5x5"
