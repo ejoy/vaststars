2c3e66254bbb59e5d218ec85183dc59305596af9
kinbei <kinbei@qq.com>
2022-04-15 10:05:22 +0800

    code optimization

--- packages/gamerender/dragdrop_camera_system.lua
+++ packages/gamerender/dragdrop_camera_system.lua
@@ -7,31 +7,20 @@ local math3d = require "math3d"
 local camera = ecs.require "camera"
 
 local dragdrop_camera_sys = ecs.system "dragdrop_camera_system"
-local touch_mb = world:sub {"touch"}
+local single_touch_mb = world:sub {"single_touch"}
 local begin_pos
-local touch_id
 
 function dragdrop_camera_sys:camera_usage()
     local last_move_x, last_move_y
-    for _, state, datas in touch_mb:unpack() do
-        for _, data in pairs(datas) do
-            if state == "START" then
-                if not touch_id then
-                    begin_pos = math3d.ref(camera.screen_to_world(data.x, data.y))
-                    touch_id = data.id
-                end
+    for _, state, data in single_touch_mb:unpack() do
+        if state == "START" then
+            begin_pos = math3d.ref(camera.screen_to_world(data.x, data.y))
 
-            elseif state == "MOVE" then
-                if touch_id == data.id then
-                    last_move_x, last_move_y = data.x, data.y
-                end
+        elseif state == "MOVE" then
+            last_move_x, last_move_y = data.x, data.y
 
-            elseif state == "CANCEL" or state == "END" then
-                if touch_id == data.id then
-                    begin_pos = nil
-                    touch_id = nil
-                end
-            end
+        elseif state == "CANCEL" or state == "END" then
+            begin_pos = nil
         end
     end
 
