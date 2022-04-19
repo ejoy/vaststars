e5bc3f296d4966efb3d343520330119100fd8c42
kinbei <kinbei@qq.com>
2022-04-19 17:12:45 +0800

    fix lower framebuffer size cause pickup scene object failed

--- packages/gamerender/engine/system/pickup_detect_system.lua
+++ packages/gamerender/engine/system/pickup_detect_system.lua
@@ -5,11 +5,16 @@ local w     = world.w
 local pickup_detect_sys = ecs.system "pickup_detect_system"
 local ipu = ecs.import.interface "ant.objcontroller|ipickup"
 local gesture_mb = world:sub{"gesture", "tap"}
+local mathpkg = import_package "ant.math"
+local mu      = mathpkg.util
 
 local function remap_xy(x, y)
-	local tmq = w:singleton("tonemapping_queue", "render_target:in")
-	local vr = tmq.render_target.view_rect
-	return x-vr.x, y-vr.y
+    local ratio = world.args.framebuffer.ratio
+    if ratio ~= nil and ratio ~= 1 then
+        x, y = mu.cvt_size(x, ratio), mu.cvt_size(y, ratio)
+    end
+	local vp = world.args.viewport
+	return x-vp.x, y-vp.y
 end
 
 function pickup_detect_sys:data_changed()
