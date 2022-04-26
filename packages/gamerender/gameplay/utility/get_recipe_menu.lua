85b8c4c5b241908d71b63009745552267c287cf0
kinbei <kinbei@qq.com>
2022-04-26 17:45:32 +0800

    set recipe

--- packages/gamerender/gameplay/utility/get_recipe_menu.lua
+++ packages/gamerender/gameplay/utility/get_recipe_menu.lua
@@ -4,11 +4,28 @@ local recipe_menu = config.recipe_menu
 local general = require "gameplay.utility.general"
 local has_type = general.has_type
 
+local function get_name_count(s)
+    local r = {}
+    for idx = 1, #s // 4 do
+        local id, n = string.unpack("<I2I2", s, 4 * idx - 3)
+        local typeobject = gameplay.query(id)
+        r[#r+1] = {name = typeobject.name, count = n}
+    end
+    return r
+end
+
 local recipes = {}
 for _, v in pairs(gameplay.prototype_name) do
     if has_type(v.type, "recipe") then
         recipes[v.category] = recipes[v.category] or {}
-        recipes[v.category][#recipes[v.category] + 1] = {name = v.name, order = v.order, icon = v.icon}
+        recipes[v.category][#recipes[v.category] + 1] = {
+            name = v.name,
+            order = v.order,
+            icon = v.icon,
+            time = v.time,
+            ingredients = get_name_count(v.ingredients),
+            results = get_name_count(v.results),
+        }
     end
 end
 
--- packages/resources/ui/page.lua
+++ packages/resources/ui/page.lua
@@ -13,18 +13,26 @@ function page_meta:update_view_pages()
         end
         view_page_idx = view_page_idx + 1
     end
-    self.data_source.view_items = vitems
+    self.source.view_items = vitems
+    if self.footer then
+        for i, child in ipairs(self.footer.childNodes) do
+            child.style.backgroundImage = (i == self.current_page) and 'textures/common/page1.texture' or 'textures/common/page0.texture'
+        end
+    end
+    if self.detail then
+        self.detail.style.left = (self.current_page - 1) * self.width .. self.unit
+    end
+    self.dirty()
 end
 
-function page_meta:update_virtual_pages(items)
+function page_meta:update_virtual_pages()
     local vpages = {}
     local count_per_page = self.row * self.col
-    local gapx = math.floor(math.fmod(self.contain_size[1], self.item_size) / (self.col + 1))
-    local gapy = math.floor(math.fmod(self.contain_size[2], self.item_size) / (self.row + 1))
-    self.data_source.gapx = gapx
-    self.data_source.gapy = gapy
+    local gapx = math.floor(math.fmod(self.width, self.item_size) / (self.col + 1))
+    local gapy = math.floor(math.fmod(self.height, self.item_size) / (self.row + 1))
     local offset = 0
     local index = 1
+    local items = self.source.items
     local total_item_count = #items
     local page_index = 0
     while index <= total_item_count do
@@ -48,47 +56,127 @@ function page_meta:update_virtual_pages(items)
             }
         end
         vpages[#vpages + 1] = new_page
-        offset = offset + self.contain_size[1]
+        offset = offset + self.width
         page_index = page_index + 1
         index = index + count_per_page
     end
+    self.gapx = gapx
+    self.gapy = gapy
     self.virtual_pages = vpages
-    self.data_source.virtual_pages = vpages
 end
 
-function page_meta.create(size, source)
-    local item_size = source.item_size
-    local row = math.floor(size[2] / item_size)
-    source.row_count = row
+function page_meta.create(e, source, pagefooter, dirty)
+    local width = tonumber(e.getAttribute("width"))
+    local height = tonumber(e.getAttribute("height"))
+    local item_size = tonumber(e.getAttribute("item_size"))
+    local unit = source.unit
     local page = {
+        current_time = 0,
         current_page = 1,
-        contain_size = size or {0,0},
-        item_size    = item_size,
-        col          = math.floor(size[1] / item_size),
-        row          = row,
         pos          = 0,
+        draging      = false,
+        interval     = 200,--按下到释放的时间小于0.5秒时显示详细信息
+        page_top     = 0,
+        gapx         = 0,
+        gapy         = 0,
         drag         = {mouse_pos = 0, anchor = 0, delta = 0},
-        data_source  = source,
-        virtual_pages = {}
+        virtual_pages = {},
+        row          = math.floor(height / item_size),
+        col          = math.floor(width / item_size),
+        width        = width,
+        height       = height,
+        item_size    = item_size,
+        detail_height = tonumber(e.getAttribute("detail_height")),
+        source       = source,
+        unit         = unit,
+        dirty        = dirty
     }
-    
     setmetatable(page, page_meta)
-    page:update_virtual_pages(source.items)
-    page:update_view_pages()
+    page:on_items_dirty()
+
+    e.style.overflow = 'hidden'
+    local panel = e.childNodes[1]
+    panel.addEventListener('mousedown', function(event) page:on_mousedown(event) end)
+    panel.addEventListener('mousemove', function(event) page:on_drag(event) end)
+    panel.addEventListener('mouseup', function(event) page:on_mouseup(event) end)
+    page.detail = panel.getElementById("detail")
+    page.detail.style.position = 'absolute'
+    page.detail.style.height = page.detail_height .. unit
+    page.view = e
+    page.panel = panel
+    page.view.style.width = width .. unit
+    page.view.style.height = height .. unit
+    page.panel.style.width = #page.virtual_pages * width .. unit
+    page.panel.style.height = (height - 30) .. unit
+    local footer = e.childNodes[2]
+    page.footer = footer
+    footer.style.flexDirection = 'row'
+    footer.style.justifyContent = 'center'
+    footer.style.width = '100%'
+    footer.style.height = '30px'
+    local page_count = #page.virtual_pages
+    for i = 1, page_count do
+        local newChild
+        for _, child in ipairs(pagefooter.childNodes) do
+            newChild = child.cloneNode()
+            newChild.style.backgroundImage = (i == page.current_page) and 'textures/common/page0.texture' or 'textures/common/page0.texture'
+            footer.appendChild(newChild)
+        end
+    end
     return page
 end
 
-function page_meta:set_contain_size(size)
-    self.contain_size = size
+function page_meta:on_items_dirty()
+    self:update_virtual_pages()
+    self:update_view_pages()
+end
+
+function page_meta:set_size(width, height)
+    self.width = width
+    self.height = height
 end
 
 function page_meta:get_current_page()
     return self.current_page
 end
 
+function page_meta:on_item_click(id, row, top)
+    if not self.detail then
+        return
+    end
+    if not self.draging then
+        if self.source.selected_id ~= id then
+            self:do_show_detail(true, id, row, top)
+        else
+            self:do_show_detail(not self.source.show_detail, id, row, top)
+        end
+    else
+        self:do_show_detail(false, 0, 0, 0)
+    end
+end
+
+function page_meta:do_show_detail(show, id, row, top)
+    if not self.detail then
+        return
+    end
+    local offset = 0
+    if show then
+        self.source.selected_id = id
+        self.detail.style.top = (top + self.item_size) .. self.unit
+        local dy = self.height - ((row + 1) * (self.item_size + self.gapy) + self.detail_height)
+        offset = (dy >= 0) and 0 or dy
+    else
+        self.source.selected_id = 0
+    end
+    self.panel.style.top = offset .. self.unit
+    self.source.show_detail = show
+    self.dirty()
+end
+
 function page_meta:on_mousedown(event)
     self.drag.mouse_pos = event.x
     self.drag.anchor = self.pos
+    self.draging = false
 end
 
 function page_meta:on_mouseup(event)
@@ -107,8 +195,8 @@ function page_meta:on_mouseup(event)
     if old_value ~= self.current_page then
         self:update_view_pages()
     end
-    self.pos = (1 - self.current_page) * self.contain_size[1]
-    event.current.style.left = tostring(self.pos) .. 'px'
+    self.pos = (1 - self.current_page) * self.width
+    self.panel.style.left = tostring(self.pos) .. self.unit
     return old_value ~= self.current_page
 end
 
@@ -116,13 +204,13 @@ function page_meta:on_drag(event)
     if event.button then
         self.drag.delta = event.x - self.drag.mouse_pos
         self.pos = self.drag.anchor + self.drag.delta
-        --event.current:setPropertyImmediate("left", tostring(math.floor(self.pos)) .. 'px')
-        local e = event.current
+        --console.log("self.pos : ", self.pos)
+        local e = self.panel
         local oldClassName = e.className
         e.className = e.className .. " notransition"
-        e.style.left = tostring(math.floor(self.pos)) .. 'px'
+        e.style.left = tostring(math.floor(self.pos)) .. self.unit
         e.className = oldClassName
-        self.data_source.draging = true
+        self.draging = true
     else
         self.drag.delta = 0
     end
