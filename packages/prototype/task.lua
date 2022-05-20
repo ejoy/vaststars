78af78fe146a14540f475154c6d77174194b2ec2
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-05-20 16:35:28 +0800

    update

--- packages/prototype/task.lua
+++ packages/prototype/task.lua
@@ -18,5 +18,27 @@ prototype "生产铁片" {
         {"textures/construct/steel-beam.texture"},
         {"textures/construct/steel-beam.texture"},
     },
+    condition = {
+        {"pretask",{"制造组装机","制造化工厂"}},
+        {"item",{{"铁片",10},{"铁丝",40}}},
+        {"tech",{"铁熔炼","化工"}},
+        {"build",{{"组装机I",2},{"机器爪I",2}}},
+    },
+    requirement = {
+        --生产指定数量道具
+        {"item",{{"铁片",10},{"铁丝",40}}},
+
+        --研究指定科技
+        {"tech",{"铁熔炼","化工"}},
+
+        --建造指定数量建筑
+        {"build",{{"组装机I",2},{"机器爪I",2}}},
+
+        --迁移指定物品(迁入/迁出指挥中心)
+        {"movetoheadquarter",{{"组装机I",2},{"机器爪I",2}}},
+        {"movefromheadquarter",{{"组装机I",2},{"机器爪I",2}}},
+    },
+
+    
     description = "使用熔炼炉生产100个铁片",
 }
