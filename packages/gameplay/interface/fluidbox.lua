874b86817367c08c0e5a71390b3c1b891bd65893
actboy168 <actboy168@gmail.com>
2022-05-26 09:38:01 +0800

    update fluidbox

--- packages/gameplay/interface/fluidbox.lua
+++ packages/gameplay/interface/fluidbox.lua
@@ -8,26 +8,43 @@ local function fluidId(name)
     return 0
 end
 
-local function update(e, pt, fluids)
+local function update_fluidboxes(e, pt, fluids)
     assert(e.fluidboxes ~= nil)
+    assert(pt.fluidboxes)
     assert(#pt.fluidboxes.input <= 4)
     assert(#pt.fluidboxes.output <= 3)
-    e.fluidbox_changed = true
     local fluidboxes = e.fluidboxes
-    for _, classify in ipairs {"in1","in2","in3","in4","out1","out2","out3"} do
+    local function getfluidId(classify)
         local what, i = classify:match "(%a*)(%d)"
         what = what.."put"
         i = tonumber(i)
         if fluids and fluids[what] and fluids[what][i] then
             local v = fluids[what][i]
-            fluidboxes[classify.."_fluid"] = fluidId(v)
-        else
-            fluidboxes[classify.."_fluid"] = 0
+            return fluidId(v)
         end
-        fluidboxes[classify.."_id"] = 0
+        return 0
+    end
+    for _, classify in ipairs {"in1","in2","in3","in4","out1","out2","out3"} do
+        local id = getfluidId(classify)
+        if id ~= fluidboxes[classify.."_fluid"] then
+            fluidboxes[classify.."_fluid"] = id
+            fluidboxes[classify.."_id"] = 0
+            e.fluidbox_changed = true
+        end
+    end
+end
+
+
+local function update_fluidbox(e, fluid)
+    assert(e.fluidbox ~= nil)
+    if e.fluidbox.fluid ~= fluid then
+        e.fluidbox.fluid = fluid
+        e.fluidbox.id = 0
+        e.fluidbox_changed = true
     end
 end
 
 return {
-    update = update
+    update_fluidboxes = update_fluidboxes,
+    update_fluidbox = update_fluidbox,
 }
