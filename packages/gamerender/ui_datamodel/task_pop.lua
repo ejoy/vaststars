ca95e5f2c8a12618a3c2cf57deda5c54e561d942
aimoonchen <kungfuchen@gmail.com>
2022-06-01 17:18:45 +0800

    task pop ui

--- packages/gamerender/ui_datamodel/task_pop.lua
+++ packages/gamerender/ui_datamodel/task_pop.lua
@@ -6,13 +6,14 @@ local M = {}
 
 function M:create(object_id)
     local current_tech = global.science.current_tech
+    local tips = {}
+    if current_tech then
+        for _, value in ipairs(current_tech.detail.tips_pic) do
+            tips[#tips+1] = {icon = value}
+        end
+    end
     return {
-        items = {
-            {icon = "textures/build_background/pic_assemble.texture"},
-            {icon = "textures/build_background/pic_assemble.texture"},
-            {icon = "textures/build_background/pic_assemble.texture"},
-            {icon = "textures/build_background/pic_assemble.texture"}
-        },
+        items = tips,
         task_name = current_tech and current_tech.name or "任务名称",
         task_desc = current_tech and current_tech.detail.desc or "任务描述",
         current_count = current_tech and current_tech.progress or 0,
