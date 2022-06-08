69bfa3b3df64fe3d694f5c639a409a7c1e264200
aimoonchen <kungfuchen@gmail.com>
2022-06-08 15:11:05 +0800

    fix tash ui bug

--- packages/gamerender/ui_datamodel/task_pop.lua
+++ packages/gamerender/ui_datamodel/task_pop.lua
@@ -2,24 +2,31 @@ local ecs, mailbox = ...
 local world = ecs.world
 local w = world.w
 local gameplay_core = require "gameplay.core"
+local iprototype = require "gameplay.interface.prototype"
 local global = require "global"
 local close_taskui_event = mailbox:sub {"close_taskui"}
 local M = {}
 
+local function get_multiple(task)
+    return string.unpack("<I2", task, 3)
+end
+
 function M:create(object_id)
     local current_tech = global.science.current_tech
     local tips = {}
-    if current_tech then
+    if current_tech and current_tech.detail.tips_pic then
         for _, value in ipairs(current_tech.detail.tips_pic) do
             tips[#tips+1] = {icon = value}
         end
     end
+    local multiple = get_multiple(current_tech.detail.task)
+    local total = current_tech and (current_tech.detail.count * 10^multiple) or 100
     return {
         items = tips,
         task_name = current_tech and current_tech.name or "任务名称",
         task_desc = current_tech and current_tech.detail.sign_desc[1].desc or "任务描述",
         current_count = current_tech and current_tech.progress or 0,
-        total_count = current_tech and current_tech.detail.count or 100
+        total_count = total
     }
 end
 
