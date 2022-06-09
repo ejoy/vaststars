ce6925ec79ec95e0f48762e1b7b5fa439844af0f
actboy168 <actboy168@gmail.com>
2022-06-09 14:46:20 +0800

    update manual

--- clibs/gameplay/src/system/manual.h
+++ clibs/gameplay/src/system/manual.h
@@ -6,7 +6,7 @@
 struct manual_container : public flatmap<uint16_t, uint16_t> {
     using mybase = flatmap<uint16_t, uint16_t>;
     bool pickup(recipe_items& r);
-    bool place(recipe_items& r);
+    void place(recipe_items& r);
 
     uint16_t pickup(uint16_t item, uint16_t amount);
     void     place(uint16_t item, uint16_t amount);
@@ -25,6 +25,7 @@ struct manual_crafting {
     std::vector<todo> todos;
     manual_container  container;
 
-    void next(ecs::manual& m);
+    void next();
+    void sync(ecs::manual& m);
     bool rebuild(lua_State* L, world& w, int id);
 };
