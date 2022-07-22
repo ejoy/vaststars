5b8a548a815ab37e0a2aea2b6f5a7fc8ba0a5f7b
actboy168 <actboy168@gmail.com>
2022-07-22 09:44:33 +0800

    kill warning

--- clibs/gameplay/src/core/container.cpp
+++ clibs/gameplay/src/core/container.cpp
@@ -26,7 +26,7 @@ chest_container::chest_container(uint16_t size)
 
 container::slot chest_container::get(uint16_t index) {
     if (index >= slots.size()) {
-        return {0,0,0};
+        return {{0,0},0};
     }
     return {slots[index], 0};
 }
