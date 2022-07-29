76df56e4b95478f7811b8a2559aa7c3eb5017a03
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-07-29 16:38:05 +0800

    modify bound

--- packages/resources/ui/lua/popup_bound.lua
+++ packages/resources/ui/lua/popup_bound.lua
@@ -1,9 +1,9 @@
 -- the unit is vmin
 local bound = {
     left = 35, -- see also: width of the detail panel in detal_panel.rml
-    top = 0,
+    top = 8,
     right = 172 - 16, -- 172vmin approximate to 97vmax, 97vmax is the width of div, 16vmin is the width of button, see also: construct.rml - the current technology progress on top right of the screen
-    bottom = 100 - 20, -- 20vmin is the height of main-menu-block, see also: construct.rml - main-menu-block
+    bottom = 100 - 25, -- 20vmin is the height of main-menu-block, see also: construct.rml - main-menu-block
 }
 
 return function (left, top, offset_left, offset_top, panel_width, panel_height)
