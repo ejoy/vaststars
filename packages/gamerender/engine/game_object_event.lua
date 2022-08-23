8fde7eab3cf27d55bc05e292cfc77d1840152f04
kinbei <kinbei@qq.com>
2022-08-23 09:55:09 +0800

    fix bug

--- packages/gamerender/engine/game_object_event.lua
+++ packages/gamerender/engine/game_object_event.lua
@@ -5,16 +5,20 @@ local w = world.w
 local imaterial = ecs.import.interface "ant.asset|imaterial"
 
 local events = {}
-events["set_material_property"] = function(prefab, ...)
+events["set_material_property"] = function(prefab, inner, ...)
     for _, eid in ipairs(prefab.tag["*"]) do
         local e <close> = w:entity(eid, "material?in")
         if e.material then
-            local who, what = ... -- TODO: check property type of material?
-            w:extend(e, "filter_material:in")
-            local fm = e.filter_material
-            if fm.main_queue[who] then
-                imaterial.set_property(e, ...)
-            end
+            imaterial.set_property(e, ...)
+        end
+    end
+end
+
+events["set_tag_material_property"] = function(prefab, inner, tag, ...)
+    for _, eid in ipairs(inner.tags[tag] or {}) do
+        local e <close> = w:entity(eid, "material?in")
+        if e.material then
+            imaterial.set_property(e, ...)
         end
     end
 end
