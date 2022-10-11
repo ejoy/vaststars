a7236d172f625937b5b9a961c22c0b5fdd1fe12a
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-10-11 10:14:23 +0800

    update

--- packages/prototype/guide.lua
+++ packages/prototype/guide.lua
@@ -5,8 +5,8 @@ local guide = {
             {"哔哩..哔哗..已迫降在{/g 代号P4031}的星球。尚未发现任何生命迹象..(失望)", "textures/guide/guide-6.texture"},
             {"哔哩..哔哗..哔哩..启动大气分析协议中..P4031{/g 缺少氧气}..(失望)"},
             {"哔哩..哔哗..哔哩..启动地质分析协议中..P4031{/g 铁铝丰富}..(轻松)","textures/guide/guide-1.texture"},
-            {"哔哩..哔哗..启动生存可靠性分析..0.04565%存活概率..(情绪表达跳过中)","textures/guide/guide-4.texture"},
-            {"博士，作为助理AI，我建议你立刻开始工作..哔哩..你的剩余生存时间理论上只有348.26地球小时..(担忧)", "textures/guide/guide-6.texture"},
+            {"哔哩..哔哗..启动生存可靠性分析..0.04565%{/g 存活概率}..(情绪表达跳过中)","textures/guide/guide-4.texture"},
+            {"博士，作为助理AI，我建议你立刻开始工作..哔哩..你的剩余生存时间理论上只有348.26{/g 地球小时}..(担忧)", "textures/guide/guide-6.texture"},
             {"迫降不算太糟糕，我们先{/g 清理指挥中心周边残骸}，兴许能找到一些有用的物资..(期待)", "textures/guide/guide-2.texture"},
         },
         narrative_end = {
@@ -57,7 +57,7 @@ local guide = {
             {"哔哩..整个基地的用电设施都需要被{/g 电网覆盖}，否则它们将无法工作..哔哩(无奈）", "textures/guide/guide-6.texture"},
             {"指挥中心可产生电力..哔哩..{/g 放置电线杆}把指挥中心的电力扩散出去..哔哩(期待)", "textures/guide/guide-3.texture"},
             {"当电线杆之间出现{/g 红色电线}时则表示成功连接，在电线{/g 蓝色范围}内就会有电力传输(兴奋)", "textures/guide/guide-3.texture"},
-            {"若指挥中心和电线杆形成一个电网，处于电网内的用电设施就可以工作了..哔哩(兴奋)", "textures/guide/guide-3.texture"},
+            {"若指挥中心和电线杆形成一个{/g 电网}，处于{/g 电网}内的用电设施就可以工作了..哔哩(兴奋)", "textures/guide/guide-3.texture"},
         },
         narrative_end = {
             guide_progress = 10,
@@ -74,7 +74,7 @@ local guide = {
     {
         name = "guide-5",
 		narrative = {
-            {"哔哩..我们终于恢复了研究能力..哔哩..目前存活概率提升为0.07672%..(轻松)", "textures/guide/guide-1.texture"},
+            {"哔哩..我们终于恢复了研究能力..哔哩..目前{/g 存活概率}提升为0.07672%..(轻松)", "textures/guide/guide-1.texture"},
             {"哔哩..让我们点击{/g 研究中心}按钮，开始研究第一个科技..(兴奋)", "textures/guide/guide-3.texture"},
         },
         narrative_end = {
@@ -108,7 +108,7 @@ local guide = {
     {
         name = "guide-7",
 		narrative = {
-            {"哔哩..哔哩..矿物采集进展非常顺利，存活概率大幅提升至0.3244%！(兴奋)", "textures/guide/guide-3.texture"},
+            {"哔哩..哔哩..矿物采集进展非常顺利，{/g 存活概率}大幅提升至0.3244%！(兴奋)", "textures/guide/guide-3.texture"},
             {"哔哩..指挥中心有{/g 制造舱}，可以生产简单物件..请使用制造舱生产几个{/g 地质科技包}..哔哩(期待)", "textures/guide/guide-2.texture"},
         },
         narrative_end = {
@@ -126,7 +126,7 @@ local guide = {
     {
         name = "guide-8",
 		narrative = {
-            {"组装机可使用3D打印技术制造地质科技包..哔哩..哔哗..请求建造{/g 组装机}..(兴奋)", "textures/guide/guide-3.texture"},
+            {"{/g 组装机}可使用3D打印技术制造地质科技包..哔哩..哔哗..请求建造{/g 组装机}..(兴奋)", "textures/guide/guide-3.texture"},
         },
         narrative_end = {
             guide_progress = 30,
