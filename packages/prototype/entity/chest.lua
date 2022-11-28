353683d42593b429ee1ddefefa0c1fe64b9462c1
uglyfrog78 <rendong.ren@alibaba-inc.com>
2022-11-28 11:37:38 +0800

    add new icons

--- packages/prototype/entity/chest.lua
+++ packages/prototype/entity/chest.lua
@@ -33,3 +33,14 @@ prototype "大铁制箱子I" {
     area = "2x2",
     slots = 30,
 }
+
+prototype "仓库" {
+    model = "prefabs/small-chest.prefab",
+    icon = "textures/building_pic/small_pic_chest.texture",
+    background = "textures/build_background/pic_chest.texture",
+    construct_detector = {"exclusive"},
+    type = {"entity"},
+    group = {"物流" , "默认"},
+    area = "5x5",
+    slots = 60,
+}
