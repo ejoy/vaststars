9aeb1b3cbf9d54e88a9d3fdace8f15f9680b60ff
actboy168 <actboy168@gmail.com>
2022-12-09 15:26:53 +0800

    cleanup

--- packages/gameplay/type/powergrid.lua
+++ packages/gameplay/type/powergrid.lua
@@ -51,19 +51,3 @@ function accumulator:ctor(init, pt)
 		accumulator = true,
 	}
 end
-
-local STATUS_IDLE <const> = 0
-local STATUS_DONE <const> = 1
-
-local function createChest(world, type, s)
-    local chest = {}
-    for idx = 2, #s//4 do
-        local id, n = string.unpack("<I2I2", s, 4*idx-3)
-        chest[#chest+1] = world:chest_slot {
-            type = type,
-            item = id,
-            limit = n * 2,
-        }
-    end
-    return table.concat(chest)
-end
