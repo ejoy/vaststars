5fe9e6014414840eabe7ae843b55b3226be359a8
von <1052019421@qq.com>
2023-04-04 15:04:26 +0800

    update printer

--- startup/pkg/mod.printer/printer.lua
+++ startup/pkg/mod.printer/printer.lua
@@ -10,8 +10,14 @@ local timer 	= ecs.import.interface "ant.timer|itimer"
 local printer_sys = ecs.system 'printer_system'
 local iprinter = ecs.interface "iprinter"
 
+
+
+
+
+
 function printer_sys:data_changed()
 end
+
 
 --[[ function printer_sys:follow_transform_updated()
     for e in w:select "printer:in bounding?in" do
@@ -28,11 +34,26 @@ end
 
 end ]]
 
+
+
 function iprinter.update_printer_percent(eid, percent)
-    local e <close> = w:entity(eid, "printer:update bounding?in")
-    assert(percent <= 1.0 and percent >= 0.0)
-    local aabb = e.bounding.scene_aabb
-    local factor = math3d.lerp(math3d.array_index(aabb, 1), math3d.array_index(aabb, 2), percent)
-    imaterial.set_property(e, "u_printer_factor", factor)
-    e.printer.percent = percent
+    for e in w:select "printer:update bounding?in" do
+        local printer = e.printer
+        if printer.eid == eid then
+            assert(percent <= 1.0 and percent >= 0.0)
+            local aabb = e.bounding.scene_aabb
+            local topy = math3d.index(math3d.array_index(aabb, 2), 2)
+            local boty = math3d.index(math3d.array_index(aabb, 1), 2)
+            local cury = percent * (topy - boty) + boty
+            local offy
+            if cury + 0.1 * (topy - boty) >= topy then
+                offy = topy - cury
+            else
+                offy = 0.1 * (topy - boty)
+            end
+            local factor = math3d.vector(offy, cury, 0, 0)
+            imaterial.set_property(e, "u_printer_factor", factor)
+            printer.percent = percent  
+        end
+    end    
 end
