244053ceea3c14f85284f6d062ca5834496b4cdb
von <1052019421@qq.com>
2023-04-14 20:09:47 +0800

    move mod to startup

--- startup/pkg/mod.terrain/package.lua
+++ startup/pkg/mod.terrain/package.lua
@@ -2,7 +2,6 @@ return {
     name = "mod.terrain",
     dependencies = {
         "ant.render",
-        "ant.render",
         "ant.math",
         "ant.camera"
     }
---
+++ startup/pkg/vaststars.mod.test/init_system.lua
@@ -0,0 +1,82 @@
+local ecs   = ...
+local world = ecs.world
+local w     = world.w
+
+local math3d = require "math3d"
+local bgfx = require "bgfx"
+
+local mathpkg = import_package "ant.math"
+local mu, mc = mathpkg.util, mathpkg.constant
+
+local renderpkg = import_package "ant.render"
+local declmgr = renderpkg.declmgr
+
+local assetmgr = import_package "ant.asset"
+local imaterial = ecs.import.interface "ant.asset|imaterial"
+local iterrain  = ecs.import.interface "mod.terrain|iterrain"
+local iprinter= ecs.import.interface "mod.printer|iprinter"
+local printer_percent = 1.0
+local printer_eid
+local istonemountain= ecs.import.interface "mod.stonemountain|istonemountain"
+local S = ecs.system "init_system"
+
+local iom = ecs.import.interface "ant.objcontroller|iobj_motion"
+
+local function create_instance(prefab, on_ready)
+    local p = ecs.create_instance(prefab)
+    p.on_ready = on_ready
+    world:create_object(p)
+end
+
+function S.init()
+    ecs.create_instance "/pkg/vaststars.mod.test/assets/skybox.prefab"
+end
+
+
+function S.init_world()
+    local mq = w:first("main_queue camera_ref:in")
+    local eyepos = math3d.vector(0, 8, -8)
+    local camera_ref<close> = w:entity(mq.camera_ref)
+    iom.set_position(camera_ref, eyepos)
+    local dir = math3d.normalize(math3d.sub(mc.ZERO_PT, eyepos))
+    iom.set_direction(camera_ref, dir)
+
+    iterrain.gen_terrain_field(256, 256, 128)
+    istonemountain.create_sm_entity(0.8, 256, 256, 128)
+    printer_eid = ecs.create_entity {
+        policy = {
+            "ant.render|render",
+            "ant.general|name",
+            "mod.printer|printer",
+        },
+        data = {
+            name        = "printer_test",
+            scene  = {s = 0.5, t = {0, 0, 0}},
+            material    = "/pkg/mod.printer/assets/printer.material",
+            visible_state = "main_view",
+            mesh        = "/pkg/vaststars.mod.test/assets/chimney-1.glb|meshes/Plane_P1.meshbin",
+            render_layer= "postprocess_obj",
+            printer = {
+                percent  = printer_percent
+            }
+        },
+    }
+end
+
+local kb_mb = world:sub{"keyboard"}
+
+function S:data_changed()
+    for _, key, press in kb_mb:unpack() do
+        if key == "M" and press == 0 then
+            printer_percent = printer_percent + 0.1
+            if printer_percent >= 1.0 then
+                printer_percent = 0.0
+            end
+            iprinter.update_printer_percent(printer_eid, printer_percent)
+        end
+    end
+end
+
+function S:camera_usage()
+ 
+end
