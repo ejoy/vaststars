1423f5a3c2efb0952aac08ea0f032c7ba10ba3cf
junjie020 <kitchen.gz.020@gmail.com>
2023-04-24 17:08:28 +0800

    separate day and night data

--- startup/pkg/mod.daynight/daynight.lua
+++ startup/pkg/mod.daynight/daynight.lua
@@ -35,57 +35,81 @@ local iom       = ecs.import.interface "ant.objcontroller|iobj_motion"
 --     math3d.ref(math3d.vector(0.7, 0.7, 0.7, 1.0)),
 -- }
 
-local DIRECT_COLORS, INDIRECT_COLORS = {}, {}
+local DAYNIGHT = {
+    DAY = {
+        DIRECT_COLORS = {},
+        INDIRECT_COLORS = {},
+    },
+    NIGHT = {
+        DIRECT_COLORS = {},
+        INDIRECT_COLORS = {},
+    }
+}
 
 local dn_sys = ecs.system "daynight_system"
 
-local DEFAULT_DIRECTIONAL_LIGHT_INTENSITY
+local function read_image_content(p)
+    local f<close> = fs.open(fs.path(p), "rb")
+    local c = f:read "a"
+    return image.parse(c, true)
+end
 
-local function read_colors_from_files()
+local function read_colors_from_files(srcfiles)
+    local direct_info, direct_c         = read_image_content(srcfiles.direct)
+    local indirect_info, indirect_c     = read_image_content(srcfiles.indirect)
+    local intensity_info, intensity_c   = read_image_content(srcfiles.intensity)
 
-    local function read_image_content(p)
-        local f<close> = fs.open(fs.path(p), "rb")
-        local c = f:read "a"
-        return image.parse(c, true)
-    end
-    do
-        local direct_info, direct_c         = read_image_content "/pkg/mod.daynight/assets/light/direct.pngx"
-        local indirect_info, indirect_c     = read_image_content "/pkg/mod.daynight/assets/light/indirect.pngx"
-        local intensity_info, intensity_c   = read_image_content "/pkg/mod.daynight/assets/light/intensity.pngx"
+    assert(direct_info.depth == 1 and (not direct_info.cubemap))
+    assert(indirect_info.depth == 1 and (not indirect_info.cubemap))
 
-        assert(direct_info.depth == 1 and (not direct_info.cubemap))
-        assert(indirect_info.depth == 1 and (not indirect_info.cubemap))
+    assert(indirect_info.width == direct_info.width and indirect_info.width == intensity_info.width)
 
-        assert(indirect_info.width == direct_info.width and indirect_info.width == intensity_info.width)
+    local direct_step<const>    = direct_info.bitsPerPixel // 8
+    local indirect_step<const>  = indirect_info.bitsPerPixel // 8
+    local intensity_step<const> = intensity_info.bitsPerPixel // 8
 
-        local direct_step<const>    = direct_info.bitsPerPixel // 8
-        local indirect_step<const>  = indirect_info.bitsPerPixel // 8
-        local intensity_step<const> = intensity_info.bitsPerPixel // 8
-
-        local function to_float(v, ...)
-            if v then
-                return v / 255.0, to_float(...)
-            end
-        end
-
-        --we just need a row
-        local direct_offset, indirect_offset, intensity_offset = 1, 1, 1
-        for iw=1, direct_info.width do
-            local r, g, b = to_float(('BBB'):unpack(direct_c, direct_offset))
-            local intensity = to_float(('B'):unpack(intensity_c, intensity_offset))
-            DIRECT_COLORS[iw] = math3d.ref(math3d.vector(r, g, b, intensity))
-
-            local ir, ig, ib = to_float(('BBB'):unpack(indirect_c, indirect_offset))
-            INDIRECT_COLORS[iw] = math3d.ref(math3d.vector(ir, ig, ib, 0.0))
-            direct_offset = direct_offset + direct_step
-            intensity_offset = intensity_offset + intensity_step
-            indirect_offset = indirect_offset + indirect_step
+    local function to_float(v, ...)
+        if v then
+            return v / 255.0, to_float(...)
         end
     end
+
+    local directcolors, indirectcolors = {}, {}
+
+    --we just need a row
+    local direct_offset, indirect_offset, intensity_offset = 1, 1, 1
+    for iw=1, direct_info.width do
+        local r, g, b = to_float(('BBB'):unpack(direct_c, direct_offset))
+        local intensity = to_float(('B'):unpack(intensity_c, intensity_offset))
+        directcolors[iw] = math3d.ref(math3d.vector(r, g, b, intensity))
+
+        local ir, ig, ib = to_float(('BBB'):unpack(indirect_c, indirect_offset))
+        indirectcolors[iw] = math3d.ref(math3d.vector(ir, ig, ib, 0.0))
+        direct_offset = direct_offset + direct_step
+        intensity_offset = intensity_offset + intensity_step
+        indirect_offset = indirect_offset + indirect_step
+    end
+
+    return directcolors, indirectcolors
 end
 
 function dn_sys:init()
-    read_colors_from_files()
+    DAYNIGHT.DAY.DIRECT_COLORS, DAYNIGHT.DAY.INDIRECT_COLORS = 
+        read_colors_from_files{
+            direct = "/pkg/mod.daynight/assets/light/day_direct.pngx",
+            indirect = "/pkg/mod.daynight/assets/light/day_indirect.pngx",
+            intensity = "/pkg/mod.daynight/assets/light/day_intensity.pngx",
+        }
+
+    DAYNIGHT.NIGHT.DIRECT_COLORS, DAYNIGHT.NIGHT.INDIRECT_COLORS = 
+        read_colors_from_files{
+            direct = "/pkg/mod.daynight/assets/light/day_direct.pngx",
+            indirect = "/pkg/mod.daynight/assets/light/day_indirect.pngx",
+            intensity = "/pkg/mod.daynight/assets/light/day_intensity.pngx",
+            -- direct = "/pkg/mod.daynight/assets/light/night_direct.pngx",
+            -- indirect = "/pkg/mod.daynight/assets/light/night_indirect.pngx",
+            -- intensity = "/pkg/mod.daynight/assets/light/night_intensity.pngx",
+        }
 end
 
 local function interpolate_in_array(t, arrays)
@@ -110,12 +134,18 @@ function dn_sys:entity_init()
         if not dn.cycle then
             dn.cycle = 0
         end
-        if dn.start_rotator then
-            update_rotation_frame(math3d.quaternion(dn.start_rotator))
-        else
-            dn.start_dir = math3d.mark(mc.NXAXIS)
-            dn.normal = math3d.mark(mc.ZAXIS)
+
+        local function init_rotator(r)
+            if r.start_rotator then
+                update_rotation_frame(math3d.quaternion(r.start_rotator))
+            else
+                r.start_dir = math3d.mark(mc.NXAXIS)
+                r.normal = math3d.mark(mc.ZAXIS)
+            end
         end
+
+        init_rotator(dn.day)
+        init_rotator(dn.night)
 
         if not dn.intensity then
             dn.intensity = ilight.default_intensity "directional"
@@ -131,12 +161,11 @@ function dn_sys:entity_remove()
     end
 end
 
-local function update_daynight_value(dne)
-    local dn = dne.daynight
-    local tc = dn.cycle
+local function update_cycle(cycle, cyclevalue, COLOR_VALUES)
+    local tc = cycle
 
     --interpolate indirect light color
-    local modulate_color = interpolate_in_array(tc, INDIRECT_COLORS)
+    local modulate_color = interpolate_in_array(tc, COLOR_VALUES.INDIRECT_COLORS)
     local sa = imaterial.system_attribs()
     sa:update("u_indirect_modulate_color", modulate_color)
 
@@ -144,24 +173,17 @@ local function update_daynight_value(dne)
     local dl = w:first "directional_light light:in scene:in"
     if dl then
         do
-            local c<const> = interpolate_in_array(tc, DIRECT_COLORS)
+            local c<const> = interpolate_in_array(tc, COLOR_VALUES.DIRECT_COLORS)
             local r, g, b, i = math3d.index(c, 1, 2, 3, 4)
             ilight.set_color_rgb(dl, r, g, b)
 
-            ilight.set_intensity(dl, i * dn.intensity)
+            ilight.set_intensity(dl, i * cyclevalue.intensity)
         end
 
         assert(0.0 <= tc and tc <= 1.0, "Invalid time cycle")
-        local ntc = tc
-        if ntc > 0.5 then
-            -- it's a moon time
-            ntc = ntc - 0.5
-        end
 
-        ntc = ntc * 2
-
-        local q = math3d.quaternion{axis=dn.normal, r=math.pi*ntc}
-        iom.set_direction(dl, math3d.transform(q, dn.start_dir, 0))
+        local q = math3d.quaternion{axis=cyclevalue.normal, r=math.pi*tc}
+        iom.set_direction(dl, math3d.transform(q, cyclevalue.start_dir, 0))
         w:submit(dl)
 
         --print("cycle:", tc, "intensity:", l, "direction:", math3d.tostring(math3d.transform(q, dnl.start_dir, 0)), "modulate color:", math3d.tostring(modulate_color))
@@ -170,9 +192,12 @@ end
 
 
 local idn = ecs.interface "idaynight"
-function idn.update_cycle(e, cycle)
-    e.daynight.cycle = cycle
-    update_daynight_value(e)
+function idn.update_day_cycle(e, cycle)
+    update_cycle(cycle, e.daynight.day, DAYNIGHT.DAY)
+end
+
+function idn.update_night_cycle(e, cycle)
+    update_cycle(cycle, e.daynight.night, DAYNIGHT.NIGHT)
 end
 
 function idn.set_rotation(e, rotator)
