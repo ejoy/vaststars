31bc0663940d870b237cbfef5e2e104869abf277
uglyfrog78 <rendong.ren@alibaba-inc.com>
2023-05-10 14:35:14 +0800

    update

---
+++ startup/pkg/vaststars.prototype/template/pipe.lua
@@ -0,0 +1,450 @@
+local entities = { {
+    dir = "N",
+    items = {},
+    prototype_name = "指挥中心",
+    recipe = "车辆装配",
+    x = 126,
+    y = 120
+  }, {
+    dir = "N",
+    items = { { "收货车站", 2 }, { "送货车站", 2 }, { "铁制电线杆", 10 }, { "熔炼炉I", 2 }, { "无人机仓库", 5 }, { "水电站I", 2 } },
+    prototype_name = "机身残骸",
+    x = 107,
+    y = 134
+  }, {
+    dir = "S",
+    items = { { "无人机仓库", 4 }, { "采矿机I", 2 }, { "科研中心I", 1 }, { "组装机I", 4 } },
+    prototype_name = "机尾残骸",
+    x = 110,
+    y = 120
+  }, {
+    dir = "S",
+    items = { { "风力发电机I", 1 }, { "蓄电池I", 10 }, { "运输车框架", 4 }, { "太阳能板I", 6 }, { "蒸汽发电机I", 8 }, { "锅炉I", 4 } },
+    prototype_name = "机翼残骸",
+    x = 133,
+    y = 122
+  }, {
+    dir = "W",
+    items = { { "化工厂I", 3 }, { "地下水挖掘机", 4 }, { "电解厂I", 1 }, { "空气过滤器I", 4 } },
+    prototype_name = "机头残骸",
+    x = 125,
+    y = 108
+  }, {
+    dir = "N",
+    prototype_name = "风力发电机I",
+    x = 119,
+    y = 121
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 117,
+    y = 122
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 117,
+    y = 130
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 117,
+    y = 135
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 117,
+    y = 140
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 117,
+    y = 145
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 117,
+    y = 150
+  }, {
+    dir = "N",
+    fluids = {
+      input = {},
+      output = { "地下卤水" }
+    },
+    prototype_name = "地下水挖掘机",
+    recipe = "离岸抽水",
+    x = 119,
+    y = 127
+  }, {
+    dir = "N",
+    fluid = "地下卤水",
+    prototype_name = "液罐I",
+    x = 133,
+    y = 127
+  }, {
+    dir = "N",
+    fluid = "地下卤水",
+    prototype_name = "液罐I",
+    x = 133,
+    y = 131
+  }, {
+    dir = "N",
+    fluid = "",
+    prototype_name = "液罐I",
+    x = 133,
+    y = 135
+  }, {
+    dir = "N",
+    fluid = "地下卤水",
+    prototype_name = "液罐I",
+    x = 133,
+    y = 139
+  }, {
+    dir = "N",
+    fluid = "地下卤水",
+    prototype_name = "液罐I",
+    x = 133,
+    y = 143
+  }, {
+    dir = "N",
+    fluids = {
+      input = {},
+      output = { "地下卤水" }
+    },
+    prototype_name = "地下水挖掘机",
+    recipe = "离岸抽水",
+    x = 119,
+    y = 131
+  }, {
+    dir = "N",
+    fluids = {
+      input = {},
+      output = { "地下卤水" }
+    },
+    prototype_name = "地下水挖掘机",
+    recipe = "离岸抽水",
+    x = 119,
+    y = 135
+  }, {
+    dir = "N",
+    fluids = {
+      input = {},
+      output = { "地下卤水" }
+    },
+    prototype_name = "地下水挖掘机",
+    recipe = "离岸抽水",
+    x = 119,
+    y = 139
+  }, {
+    dir = "N",
+    fluids = {
+      input = {},
+      output = { "地下卤水" }
+    },
+    prototype_name = "地下水挖掘机",
+    recipe = "离岸抽水",
+    x = 119,
+    y = 143
+  }, {
+    dir = "N",
+    fluids = {
+      input = {},
+      output = { "地下卤水" }
+    },
+    prototype_name = "地下水挖掘机",
+    recipe = "离岸抽水",
+    x = 119,
+    y = 147
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 127,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 128,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 129,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 130,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 131,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 122,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 123,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 124,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 125,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 126,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 132,
+    y = 128
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 122,
+    y = 132
+  }, {
+    dir = "W",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 132,
+    y = 132
+  }, {
+    dir = "N",
+    fluid = "地下卤水",
+    prototype_name = "液罐I",
+    x = 122,
+    y = 135
+  }, {
+    dir = "N",
+    fluid = "地下卤水",
+    prototype_name = "液罐I",
+    x = 125,
+    y = 135
+  }, {
+    dir = "N",
+    fluid = "地下卤水",
+    prototype_name = "液罐I",
+    x = 130,
+    y = 135
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 128,
+    y = 136
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 129,
+    y = 136
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 122,
+    y = 140
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 123,
+    y = 140
+  }, {
+    dir = "W",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 132,
+    y = 140
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 124,
+    y = 140
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 125,
+    y = 140
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 122,
+    y = 144
+  }, {
+    dir = "W",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 132,
+    y = 144
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 125,
+    y = 144
+  }, {
+    dir = "W",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 124,
+    y = 144
+  }, {
+    dir = "W",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 128,
+    y = 144
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "地下管1-JI型",
+    x = 129,
+    y = 144
+  }, {
+    dir = "N",
+    fluids = {
+      input = { "地下卤水" },
+      output = { "蒸汽" }
+    },
+    prototype_name = "锅炉I",
+    recipe = "卤水沸腾",
+    x = 133,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 122,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 123,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 124,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 125,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 126,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 127,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 128,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 129,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 130,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 131,
+    y = 148
+  }, {
+    dir = "E",
+    fluid = "地下卤水",
+    prototype_name = "管道1-I型",
+    x = 132,
+    y = 148
+  }, {
+    dir = "N",
+    fluids = {
+      input = { "蒸汽" },
+      output = {}
+    },
+    prototype_name = "蒸汽发电机I",
+    recipe = "蒸汽发电",
+    x = 133,
+    y = 150
+  }, {
+    dir = "N",
+    fluids = {
+      input = { "蒸汽" },
+      output = {}
+    },
+    prototype_name = "蒸汽发电机I",
+    recipe = "蒸汽发电",
+    x = 133,
+    y = 155
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 117,
+    y = 155
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 125,
+    y = 155
+  }, {
+    dir = "N",
+    prototype_name = "铁制电线杆",
+    x = 132,
+    y = 155
+  } }
+local road = {}
+
+return {
+    name = "水管测试",
+    entities = entities,
+    road = road,
+}
+    
