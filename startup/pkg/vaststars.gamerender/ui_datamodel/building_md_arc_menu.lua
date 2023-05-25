4afb38358948b9f1ba441f6cb7de2876dfdbecf7
kinbei <kinbei@qq.com>
2023-05-25 16:20:05 +0800

    bugfix

--- startup/pkg/vaststars.gamerender/ui_datamodel/building_md_arc_menu.lua
+++ startup/pkg/vaststars.gamerender/ui_datamodel/building_md_arc_menu.lua
@@ -8,6 +8,7 @@ local iui = ecs.import.interface "vaststars.gamerender|iui"
 
 local move_mb = mailbox:sub {"move"}
 local teardown_mb = mailbox:sub {"teardown"}
+local copy_md = mailbox:sub {"copy"}
 
 local M = {}
 function M:create(object_id, object_position, ui_x, ui_y)
@@ -18,6 +19,7 @@ function M:create(object_id, object_position, ui_x, ui_y)
     local datamodel = {
         move = typeobject.move ~= false,
         teardown = typeobject.teardown ~= false,
+        copy = true,
         object_id = object_id,
         left = ui_x,
         top = ui_y,
@@ -34,6 +36,10 @@ function M:stage_ui_update(datamodel, object_id)
     end
     for _ in teardown_mb:unpack() do
         iui.redirect("construct.rml", "teardown", object_id)
+    end
+    for _ in copy_md:unpack() do
+        local object = assert(objects:get(object_id))
+        iui.redirect("construct.rml", "construct_entity", object.prototype_name)
     end
 end
 
