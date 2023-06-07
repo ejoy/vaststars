4473e0faa5d15ef5d14604cc8d8b9ee9355dfb6f
von <1052019421@qq.com>
2023-06-07 10:28:13 +0800

    fix crack

--- startup/pkg/mod.crack/crack.lua
+++ startup/pkg/mod.crack/crack.lua
@@ -6,7 +6,7 @@ local math3d        = require "math3d"
 local init_system = ecs.system "init_system"
 
 function init_system:entity_init()
-    for e in w:select "INIT crack:update eid:in render_layer:in" do
+    for e in w:select "INIT crack:update eid:in render_layer:in scene:in" do
         assert(e.crack and e.crack.crack_color and e.crack.crack_emissive)
         local eid = ecs.create_entity{
             policy = {
@@ -14,7 +14,7 @@ function init_system:entity_init()
                 "ant.render|render",
             },
             data = {
-                scene = {parent = e.eid},
+                scene = {s = e.scene.s, r = e.scene.r, t = e.scene.t},
                 mesh  = "/pkg/mod.crack/assets/shapes/crack.glb|meshes/Plane_P1.meshbin",
                 material    = "/pkg/mod.crack/assets/crack_color.material",
                 visible_state = "main_view|selectable",
