bb088242059839bfdd91ffe9b014a6b01d48fe0c
von <1052019421@qq.com>
2023-06-25 09:39:44 +0800

    update sm

--- startup/pkg/vaststars.mod.test/init_system.lua
+++ startup/pkg/vaststars.mod.test/init_system.lua
@@ -68,7 +68,7 @@ function S.init_world()
     iom.set_position(camera_ref, eyepos)
     local dir = math3d.normalize(math3d.sub(mc.ZERO_PT, eyepos))
     iom.set_direction(camera_ref, dir)
-    iterrain.gen_terrain_field(256, 256, 128)
+    iterrain.gen_terrain_field(64, 64, 32)
 --[[       ecs.create_entity{
         policy = {
             "ant.scene|scene_object",
@@ -163,23 +163,13 @@ function S.init_world()
         end
     end
     --iroad.update_roadnet_group(1000, create_list)   
- 
---[[      local density = 0.5
-    local width, height, offset, UNIT = 256, 256, 128, 10
-    local scale_table = {
-        big = 1.0,
-        middle = 0.6,
-        small = 0.01
-    }
-      local stone_area = {
-        {x = 0, z = 0},
-    }
-     local open_area = {
-        {x = -128, z = 128, w = 256, h = 256}
-    }   
-    istonemountain.create_sm_entity(density, width, height, offset, UNIT, scale_table, {}, {})     ]]
+    local density = 0.9
+    local width, height, offset, UNIT = 64, 64, 32, 10
+    local idx_string = istonemountain.create_random_sm(density, width, height, offset, UNIT)
+    istonemountain.create_sm_entity(idx_string)
+    --istonemountain.create_sm_entity_config(config, width, height, offset, UNIT)    
     --create_mark()
-
+    
 --[[      printer_eid = ecs.create_entity {
         policy = {
             "ant.render|render",
