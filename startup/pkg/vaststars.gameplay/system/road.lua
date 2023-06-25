ac94b7d1b862d268f1d25915c2840917b7cfc809
actboy168 <actboy168@gmail.com>
2023-06-25 09:39:58 +0800

    cleanup

--- startup/pkg/vaststars.gameplay/system/road.lua
+++ startup/pkg/vaststars.gameplay/system/road.lua
@@ -4,30 +4,6 @@ local query = prototype.queryById
 
 local m = system "road"
 
-local mt = {}
-mt.__index = function (t, k)
-    t[k] = setmetatable({}, mt)
-    return t[k]
-end
-
-local function is_roadid(pt)
-    for _, t in ipairs(pt.type) do
-        if t == "road" then
-            return true
-        end
-    end
-    return false
-end
-
-local roadbits_rev = setmetatable({}, mt)
-
-local Direction <const> = {
-    ["N"] = 0,
-    ["E"] = 1,
-    ["S"] = 2,
-    ["W"] = 3,
-}
-
 local N <const> = 0
 local E <const> = 1
 local S <const> = 2
@@ -42,21 +18,6 @@ local MapRoad <const> = {
     Endpoint     = 1 << 4,
     NoHorizontal = 1 << 5,
     NoVertical   = 1 << 6,
-}
-
--- see also clibs\gameplay\src\roadnet\type.h - enum class direction
-local RoadDirection = {
-    l = 0,
-    t = 1,
-    r = 2,
-    b = 3,
-}
-
-local DirectionToMapRoad <const> = {
-    [N] = RoadDirection.t,
-    [E] = RoadDirection.r,
-    [S] = RoadDirection.b,
-    [W] = RoadDirection.l,
 }
 
 local function pack(x, y)
@@ -83,31 +44,8 @@ local function buildingPostion(x, y, building, area)
     return building.x + dx, building.y + dy
 end
 
-local function calc_roadbits(pt, direction)
-    local bits = 0
-    for _, c in ipairs(pt.crossing.connections) do
-        local dir = (Direction[c.position[3]] + direction) % 4
-        bits = bits | (1 << (DirectionToMapRoad[dir]))
-    end
-    return bits
-end
-
 local function rotateMask(mask, dir)
     return ((mask << dir) | (mask >> (4-dir))) & 0xF
-end
-
-function m.prototype_restore(world)
-    roadbits_rev = setmetatable({}, mt)
-
-    for _, pt in pairs(prototype.all()) do
-        if is_roadid(pt) then
-            for _, dir in pairs(pt.building_direction) do
-                local bits = calc_roadbits(pt, Direction[dir])
-                assert(rawget(roadbits_rev[pt.building_category], bits) == nil)
-                roadbits_rev[pt.building_category][bits] = { prototype = pt.id, direction = Direction[dir] }
-            end
-        end
-    end
 end
 
 local DIRTY_ROADNET <const> = 1 << 4
@@ -119,76 +57,37 @@ function m.build(world)
         return
     end
 
-    do
-        local function getRoadMask(e)
-            local building = e.building
-            local pt = query(building.prototype)
-            assert(#pt.road == 4)
-            local x, y, mask = string.unpack("<I1I1I2", pt.road)
-            assert(x == 0 and y == 0)
-            return rotateMask(mask, building.direction)
-        end
-        local eid_cache = {}
-        for v in ecs:select "road building:in eid:in" do
-            local building = v.building
-            local pt = query(building.prototype)
-            local dx, dy = buildingPostion(0, 0, building, pt.area)
-            local key = pack(dx, dy)
-            eid_cache[key] = v.eid
-        end
-        for v in ecs:select "endpoint building:in eid:in" do
-            local building = v.building
-            local pt = query(building.prototype)
-            for i = 1, #pt.road, 4 do
-                local x, y, mask = string.unpack("<I1I1I2", pt.road, i)
-                local dx, dy = buildingPostion(x, y, building, pt.area)
-                local key = pack(dx, dy)
-                local eid = eid_cache[key]
-                if eid then
-                    local e = assert(world.entity[eid])
-                    local pt = prototype.queryById(e.building.prototype)
-                    local f = roadbits_rev[pt.building_category][mask | getRoadMask(e)]
-                    e.building.prototype = f.prototype
-                    e.building.direction = f.direction
-                    e.building_changed = true
-                end
-            end
+    local map = {}
+    for v in ecs:select "road building:in" do
+        local building = v.building
+        local pt = query(building.prototype)
+        for i = 1, #pt.road, 4 do
+            local x, y, mask = string.unpack("<I1I1I2", pt.road, i)
+            local dx, dy = buildingPostion(x, y, building, pt.area)
+            local mapkey = pack(dx, dy)
+            assert(not map[mapkey])
+            map[mapkey] = rotateMask(mask, building.direction)
         end
     end
-
-    do
-        local map = {}
-        for v in ecs:select "road building:in" do
-            local building = v.building
-            local pt = query(building.prototype)
-            for i = 1, #pt.road, 4 do
-                local x, y, mask = string.unpack("<I1I1I2", pt.road, i)
-                local dx, dy = buildingPostion(x, y, building, pt.area)
-                local mapkey = pack(dx, dy)
-                assert(not map[mapkey])
+    for v in ecs:select "endpoint building:in" do
+        local building = v.building
+        local pt = query(building.prototype)
+        local affected_roads_mask = 0
+        if building.direction == N or building.direction == S then
+            affected_roads_mask = MapRoad.NoVertical
+        else
+            affected_roads_mask = MapRoad.NoHorizontal
+        end
+        for i = 1, #pt.road, 4 do
+            local x, y, mask = string.unpack("<I1I1I2", pt.road, i)
+            local dx, dy = buildingPostion(x, y, building, pt.area)
+            local mapkey = pack(dx, dy)
+            if not map[mapkey] then
                 map[mapkey] = rotateMask(mask, building.direction)
-            end
-        end
-        for v in ecs:select "endpoint building:in" do
-            local building = v.building
-            local pt = query(building.prototype)
-            local affected_roads_mask = 0
-            if building.direction == N or building.direction == S then
-                affected_roads_mask = MapRoad.NoVertical
             else
-                affected_roads_mask = MapRoad.NoHorizontal
-            end
-            for i = 1, #pt.road, 4 do
-                local x, y, mask = string.unpack("<I1I1I2", pt.road, i)
-                local dx, dy = buildingPostion(x, y, building, pt.area)
-                local mapkey = pack(dx, dy)
-                if not map[mapkey] then
-                    map[mapkey] = rotateMask(mask, building.direction)
-                else
-                    map[mapkey] = map[mapkey] | rotateMask(mask, building.direction) | affected_roads_mask
-                end
+                map[mapkey] = map[mapkey] | rotateMask(mask, building.direction) | affected_roads_mask
             end
         end
-        world:roadnet_reset(map)
     end
+    world:roadnet_reset(map)
 end
