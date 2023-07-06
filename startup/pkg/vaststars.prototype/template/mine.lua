5929f0382eaef72346e03a3dc8cffc1386969a5e
uglyfrog78 <rendong.ren@alibaba-inc.com>
2023-07-06 16:06:22 +0800

    update

---
+++ startup/pkg/vaststars.prototype/template/mine.lua
@@ -0,0 +1,16 @@
+local entities = {}
+local road = {}
+
+local mineral = {
+  ["115,129"] = "碎石",
+  ["120,129"] = "铁矿石",
+  ["125,129"] = "地热气",
+  ["130,129"] = "铝矿石",
+}
+
+return {
+  name = "矿区模式",
+  entities = entities,
+  road = road,
+  mineral = mineral,
+}
