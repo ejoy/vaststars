20e1ad0da8af3f60a8a0273b5e332f8548ba411c
junjie020 <kitchen.gz.020@gmail.com>
2023-07-21 11:12:37 +0800

    fix compile error

--- startup/pkg/mod.printer/assets/printer_fs_func.sh
+++ startup/pkg/mod.printer/assets/printer_fs_func.sh
@@ -24,17 +24,21 @@ void CUSTOM_FS_FUNC(in FSInput fs_input, inout FSOutput fs_output)
         discard;
     
     int building;
-    if(v_posWS.y > u_building_topmost - u_building_offset){
+    if(fs_input.pos.y > u_building_topmost - u_building_offset){
         building = 1;
     } else{
         building = 0;
     }
 
-    if(building || (dot(input_attribs.N, input_attribs.V) < 0)) {
+    if(building) {
         fs_output.color = u_construct_color;
     } else {
         input_attributes input_attribs = (input_attributes)0;
         build_fs_input_attribs(fs_input, input_attribs);
-        fs_output.color = compute_lighting(input_attribs);
+        if (dot(input_attribs.N, input_attribs.V) < 0){
+            fs_output.color = u_construct_color;
+        } else {
+            fs_output.color = compute_lighting(input_attribs);
+        }
     }
 }
