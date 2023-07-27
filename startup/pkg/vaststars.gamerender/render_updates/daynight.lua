a99f675a1cc139ae4fc5e75296710acb7197fb87
kinbei <kinbei@qq.com>
2023-07-27 16:58:21 +0800

    cleanup

--- startup/pkg/vaststars.gamerender/render_updates/daynight.lua
+++ startup/pkg/vaststars.gamerender/render_updates/daynight.lua
@@ -3,8 +3,6 @@ local world = ecs.world
 local w = world.w
 
 local DAYNIGHT_DEBUG <const> = require "debugger".daynight
-local DuskTick <const> = require("gameplay.interface.constant").DuskTick
-local NightTick <const> = require("gameplay.interface.constant").NightTick
 local DayTick <const> = require("gameplay.interface.constant").DayTick
 local idn = ecs.import.interface "ant.daynight|idaynight"
 local gameplay_core = require "gameplay.core"
@@ -17,18 +15,8 @@ local daynight_update; do
             return false
         end
     elseif DAYNIGHT_DEBUG then
-        local function parse(s)
-            if type(s) ~= "string" or s == "" then
-                return s
-            end
-            local total_sec, day_ratio, night_ratio = s:match("(%d+):?(%d*):?(%d*)")
-            return tonumber(total_sec), tonumber(day_ratio), tonumber(night_ratio)
-        end
-
-        local total_sec, day_ratio, night_ratio = parse(DAYNIGHT_DEBUG)
-        day_ratio, night_ratio = day_ratio or 50, night_ratio or 50
+        local total_sec = tostring(DAYNIGHT_DEBUG)
         local total_ms = total_sec * 1000
-        local day_ms = total_ms * (day_ratio / (day_ratio + night_ratio))
 
         local ltask = require "ltask"
         local function gettime()
@@ -42,13 +30,7 @@ local daynight_update; do
                 return false
             end
 
-            local cycle = gettime() % total_ms
-            if cycle >= 0 and cycle < day_ms then
-                idn.update_day_cycle(dne, (cycle % day_ms)/day_ms)
-            else
-                idn.update_night_cycle(dne, (cycle - day_ms)/(total_ms - day_ms))
-            end
-
+            idn.update_cycle(dne, (gettime() % total_ms)/total_ms)
             return false
         end
     else
@@ -58,15 +40,7 @@ local daynight_update; do
                 return false
             end
 
-            local cycle = gameplayWorld:now() % DayTick
-            if cycle >= 0 and cycle < DuskTick then
-                idn.update_day_cycle(dne, (cycle % DuskTick)/DuskTick)
-            elseif cycle < NightTick then
-                idn.update_night_cycle(dne, (cycle - DuskTick)/(NightTick - DuskTick))
-            else
-                idn.update_day_cycle(dne, (cycle - NightTick)/(DayTick - NightTick))
-            end
-
+            idn.update_cycle(dne, (gameplayWorld:now() % DayTick)/DayTick)
             return false
         end
     end
