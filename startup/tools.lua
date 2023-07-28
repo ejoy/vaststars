4b1ecd75705b0727b709ea72faebfbe1a2ebac93
actboy168 <actboy168@gmail.com>
2023-07-28 10:00:12 +0800

    update tools

--- startup/tools.lua
+++ startup/tools.lua
@@ -2,10 +2,11 @@ package.path = "engine/?.lua"
 require "bootstrap"
 local task = dofile "/engine/task/bootstrap.lua"
 task {
+    support_package = true,
     service_path = "/engine/service/?.lua;${package}/service/?.lua;../../startup/?.lua;",
 	lua_path = "/engine/?.lua;",
     bootstrap = { "tools_root" },
     logger = { "logger" },
-    exclusive = { "timer"},
+    exclusive = { "timer", "subprocess" },
     debuglog = "debug.txt",
 }
