c55999f43c4c896694650b4df2ebc549cec5a107
uglyfrog78 <rendong.ren@alibaba-inc.com>
2023-08-01 10:04:32 +0800

    update icons of plumbing

--- startup/pkg/vaststars.prototype/item/plumbing.lua
+++ startup/pkg/vaststars.prototype/item/plumbing.lua
@@ -8,6 +8,7 @@ prototype "液罐I" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/hydroplant.texture",
     backpack_stack = 20,
     item_description = "用于储存液体的容器",
 }
@@ -19,6 +20,7 @@ prototype "液罐II" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/hydroplant.texture",
     backpack_stack = 20,
     item_description = "用于储存液体的容器",
 }
@@ -30,6 +32,7 @@ prototype "液罐III" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/hydroplant.texture",
     backpack_stack = 20,
     item_description = "用于储存液体的容器",
 }
@@ -41,6 +44,7 @@ prototype "气罐I" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/hydroplant.texture",
     backpack_stack = 20,
     item_description = "专门贮藏气体的容器",
 }
@@ -52,6 +56,7 @@ prototype "地下水挖掘机I" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/water-excavator.texture",
     backpack_stack = 20,
     item_description = "用于从含水层等地下水源中挖掘和提取水的机器",
 }
@@ -63,6 +68,7 @@ prototype "地下水挖掘机II" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/water-excavator.texture",
     backpack_stack = 20,
     item_description = "用于从含水层等地下水源中挖掘和提取水的机器",
 }
@@ -74,6 +80,7 @@ prototype "压力泵I" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/motor.texture",
     backpack_stack = 20,
     item_description = "用于增加流体压力的机械设备",
 }
@@ -85,6 +92,7 @@ prototype "烟囱I" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/crucible.texture",
     backpack_stack = 20,
     item_description = "用于排放工业设施中烟气或废气的设施",
 }
@@ -96,6 +104,7 @@ prototype "烟囱II" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/crucible.texture",
     backpack_stack = 20,
     item_description = "用于排放工业设施中烟气或废气的设施",
 }
@@ -107,6 +116,7 @@ prototype "排水口I" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/crucible.texture",
     backpack_stack = 20,
     item_description = "用于排放多余水分或废水的装置",
 }
@@ -118,6 +128,7 @@ prototype "排水口II" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/crucible.texture",
     backpack_stack = 20,
     item_description = "用于排放多余水分或废水的装置",
 }
@@ -129,6 +140,7 @@ prototype "空气过滤器I" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/electrolysis.texture",
     backpack_stack = 20,
     item_description = "抽取空气的装置",
 }
@@ -140,6 +152,7 @@ prototype "空气过滤器II" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/electrolysis.texture",
     backpack_stack = 20,
     item_description = "抽取空气的装置",
 }
@@ -151,6 +164,7 @@ prototype "空气过滤器III" {
     pile = "4x1x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/electrolysis.texture",
     backpack_stack = 20,
     item_description = "抽取空气的装置",
 }
@@ -162,6 +176,7 @@ prototype "管道1-X型" {
     pile = "4x4x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/capacitor.texture",
     backpack_stack = 100,
     item_description = "放置在地上传输液体或气体的管道",
 }
@@ -173,6 +188,7 @@ prototype "地下管1-JI型" {
     pile = "4x4x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/capacitor.texture",
     backpack_stack = 100,
     item_description = "放置在地下传输液体或气体的管道",
 }
@@ -184,6 +200,7 @@ prototype "地下管2-JI型" {
     pile = "4x4x4",
     drone_depot_gap3 = "0.2x0.05x0.2",
     pile_model = "prefabs/stackeditems/stack-gravel.prefab",
+    item_icon = "textures/icons/item/capacitor.texture",
     backpack_stack = 100,
     item_description = "放置在地下传输液体或气体的管道",
 }
