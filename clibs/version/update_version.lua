43ff22b76445eea90f3bf4177abf54b0bf3f1f27
actboy168 <actboy168@gmail.com>
2023-08-02 15:31:57 +0800

    fixes bug

--- clibs/version/update_version.lua
+++ clibs/version/update_version.lua
@@ -1,16 +1,20 @@
 local output = ...
-local platform = require "bee.platform"
 local subprocess = require "bee.subprocess"
 
 local function GitVersion(path)
-    local prog = assert(subprocess.spawn {
+    local prog = subprocess.spawn {
         "git", "rev-parse", "HEAD",
         cwd = path,
         stdout = true,
         searchPath = true,
-    })
+    }
+    if not prog then
+        return ""
+    end
+    if 0 ~= prog:wait() then
+        return ""
+    end
     local data = prog.stdout:read "a"
-    assert(0 == prog:wait())
     return data:match "[0-9a-f]+"
 end
 
