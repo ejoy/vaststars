d60c7b206fbac322896b8056731d4b6622e68b72
actboy168 <actboy168@gmail.com>
2023-08-03 09:27:18 +0800

    rework loading

--- startup/pkg/vaststars.gamerender/ui_datamodel/loading.lua
+++ startup/pkg/vaststars.gamerender/ui_datamodel/loading.lua
@@ -1,68 +1,29 @@
-local ecs, mailbox = ...
-local world = ecs.world
-local w = world.w
+local ecs = ...
+
 local iui = ecs.import.interface "vaststars.gamerender|iui"
+local assetmgr = import_package "ant.asset"
+local vfs = require "vfs"
+local datalist  = require "datalist"
+local ltask = require "ltask"
 
-local resources_loader = ecs.require "ui_datamodel.common.resources_loader"
-local resources = require "resources"
-
-local WorkerNum <const> = 4
-
----------------
 local M = {}
-
-local function worker(index, status)
-    while not status.stop do
-        status.current = status.current + 1
-        local filename = resources[status.current]
-        if status.current + 1 > #resources then
-            break
-        end
-        status[index] = filename
-        resources_loader.load(filename)
-    end
-    status[index] = ""
-end
-
+local WorkerNum <const> = 6
 local status
 
-function M:create(load)
-    if load == nil then
-        load = true
-    end
-
-    status = { current = 0, stop = false }
-    local ltask = require "ltask"
-    for i = 1, WorkerNum do
-        status[i] = ""
-        ltask.fork(worker, i, status)
-    end
-
-    return {
-        current = 0,
-        closed = false, -- TODO: remove this?
-        load = load,
-        status = status,
-        progress = "0%",
-    }
+local function status_addtask(task)
+    status.pending[#status.pending+1] = task
 end
 
-function M:stage_camera_usage(datamodel)
-    if datamodel.closed == true then -- prevent call load_game() when window is closed
-        return
+local function status_finish()
+    if #status.pending == 0 and status.loading == 0 then
+        return true
     end
-    if status.current + 1 > #resources then
-        iui.close("ui/loading.rml")
-        datamodel.closed = true
-        return
+    if status.stop then
+        return true
     end
-    for i, v in ipairs(status) do
-        datamodel.status[i] = v
-    end
-    datamodel.progress = string.format("%d%%", math.floor(status.current / #resources * 100))
 end
 
-local function stopped()
+local function status_stopped()
     for _, v in ipairs(status) do
         if v ~= "" then
             return
@@ -71,10 +32,122 @@ local function stopped()
     return true
 end
 
+local function touch_res(r)
+    local _ = #r
+end
+
+local function readall(path)
+    local realpath = assert(vfs.realpath(path))
+    local f <close> = assert(io.open(realpath))
+    return f:read "a"
+end
+
+local handler = {}
+
+function handler.prefab(f)
+    local prefab_data = readall(f)
+    for _, e in ipairs(datalist.parse(prefab_data)) do
+        if e.prefab then -- TODO: special case for prefab
+            goto continue
+        end
+        local data = e.data
+        if data.material then
+            local m = assetmgr.load_material(data.material)
+            assetmgr.unload_material(m)
+        end
+        if data.animation then
+            for _, v in pairs(data.animation) do
+                touch_res(assetmgr.resource(v))
+                vfs.realpath(v:match("^(.+%.).*$") .. "event")
+            end
+        end
+        if data.mesh then
+            touch_res(assetmgr.resource(data.mesh))
+        end
+        if data.meshskin then
+            touch_res(assetmgr.resource(data.meshskin))
+        end
+        if data.skeleton then
+            touch_res(assetmgr.resource(data.skeleton))
+        end
+        ::continue::
+    end
+end
+
+function handler.texture(f)
+    assetmgr.load_texture(f)
+end
+
+function handler.material(f)
+    local m = assetmgr.load_material(f)
+    assetmgr.unload_material(m)
+end
+
+local function dotask(filename)
+    local ext = filename:match(".*%.(.*)$")
+    log.info(("resources_loader|load %s"):format(filename))
+    if handler[ext] then
+        handler[ext](filename)
+    else
+        vfs.realpath(filename)
+    end
+end
+
+local function worker(index)
+    while not status_finish() do
+        local filename = table.remove(status.pending)
+        if not filename then
+            ltask.sleep(1)
+            goto continue
+        end
+        status.loading = status.loading + 1
+        status[index] = filename
+        dotask(filename)
+        status.loaded = status.loaded + 1
+        status.loading = status.loading - 1
+        status[index] = " "
+        ::continue::
+    end
+    status[index] = ""
+end
+
+function M:create()
+    status = {
+        pending = {},
+        loading = 0,
+        loaded = 0,
+        stop = false,
+    }
+
+    for _, v in ipairs(require "resources") do
+        status_addtask(v)
+    end
+
+    for i = 1, WorkerNum do
+        status[i] = ""
+        ltask.fork(worker, i)
+    end
+    return {
+        status = status,
+        progress = "0%",
+    }
+end
+
+function M:stage_camera_usage(datamodel)
+    if status_stopped() then
+        iui.close("ui/loading.rml")
+        return
+    end
+    for i, v in ipairs(status) do
+        datamodel.status[i] = v
+    end
+    local progress = status.loaded / (status.loading + #status.pending + status.loaded)
+    datamodel.progress = string.format("%d%%", math.floor(progress * 100))
+end
+
 function M:close()
-    local ltask = require "ltask"
     status.stop = true
-    while not stopped() do
+    while not status_stopped() do
         ltask.sleep(1)
     end
 end
