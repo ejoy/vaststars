41e1673c5980d15d31c51136ffe9b93c5f1eb2b3
actboy168 <actboy168@gmail.com>
2023-08-04 10:53:47 +0800

    rework loading

--- startup/pkg/vaststars.gamerender/ui_datamodel/loading.lua
+++ startup/pkg/vaststars.gamerender/ui_datamodel/loading.lua
@@ -2,7 +2,6 @@ local ecs = ...
 
 local iui = ecs.import.interface "vaststars.gamerender|iui"
 local assetmgr = import_package "ant.asset"
-local serialize = import_package "ant.serialize"
 local vfs = require "vfs"
 local ltask = require "ltask"
 local fs = require "filesystem"
@@ -16,6 +15,7 @@ local BlackList <const> = {
     ["/pkg/ant.bake"] = true,
     ["/pkg/ant.resources.binary/test"] = true,
     ["/pkg/ant.resources.binary/meshes"] = true,
+    ["/pkg/ant.efk/efkbgfx/examples"] = true,
 }
 
 local function status_addtask(task)
@@ -47,146 +47,92 @@ local function status_stopped()
     return true
 end
 
-local function touch_res(r)
-    local _ = #r
-end
+local Resource <const> = {
+    [".texture"] = true,
+    [".material"] = true,
+    [".glb"] = true,
+}
 
-local function readall(path)
-    local f <close> = assert(io.open(path))
-    return f:read "a"
-end
+local File <const> = {
+    -- ecs
+    [".prefab"] = true,
+    [".ecs"] = true,
+    -- script
+    [".lua"] = true,
+    -- ui
+    [".bundle"] = true,
+    [".rcss"] = true,
+    [".rml"] = true,
+    -- effect
+    [".efk"] = true,
+    -- font
+    [".ttf"] = true,
+    [".otf"] = true, --TODO: remove it?
+    [".ttc"] = true, --TODO: remove it?
+    -- sound
+    [".bank"] = true,
+    -- animation
+    [".event"] = true,
+    [".anim"] = true,
+    -- compiled resource
+    [".bin"] = true,
+    [".cfg"] = true,
+    [".ozz"] = true,
+    [".vbbin"] = true,
+    [".vb2bin"] = true,
+    [".ibbin"] = true,
+    [".meshbin"] = true,
+    [".skinbin"] = true,
+    --TODO: remove they
+    [".patch"] = true,
+    [""] = true,
+}
 
 local handler = {}
 
-local function parse_prefab(path, realpath)
-    for _, e in ipairs(serialize.parse(path, readall(realpath))) do
-        if e.prefab then -- TODO: special case for prefab
-            goto continue
-        end
-        local data = e.data
-        if data.material then
-            status_addtask {
-                type = "material",
-                filename = data.material,
-            }
-        end
-        if data.animation then
-            for _, v in pairs(data.animation) do
-                status_addtask {
-                    type = "resource",
-                    filename = v,
-                }
-                status_addtask {
-                    type = "file",
-                    filename = v:match("^(.+%.).*$") .. "event",
-                }
-            end
-        end
-        if data.mesh then
-            status_addtask {
-                type = "resource",
-                filename = data.mesh,
-            }
-        end
-        if data.meshskin then
-            status_addtask {
-                type = "resource",
-                filename = data.meshskin,
-            }
-        end
-        if data.skeleton then
-            status_addtask {
-                type = "resource",
-                filename = data.skeleton,
-            }
-        end
-        ::continue::
+if __ANT_RUNTIME__ then
+    function handler.file(f)
+        vfs.realpath(f)
     end
-end
-
-function handler.prefab(path)
-    local realpath = assert(vfs.realpath(path))
-    parse_prefab(path, realpath)
-end
-
-function handler.compiled_prefab(path)
-    local realpath = assert(assetmgr.compile(path))
-    parse_prefab(path, realpath)
-end
-
-function handler.glb(f)
-    status_addtask {
-        type = "compiled_prefab",
-        filename = f .. "|mesh.prefab",
-    }
-    if assetmgr.compile(f .. "|animation.prefab") then
-        status_addtask {
-            type = "compiled_prefab",
-            filename = f .. "|animation.prefab",
-        }
-    end
-end
-
-function handler.texture(f)
-    assetmgr.load_texture(f)
-end
-
-function handler.material(f)
-    local m = assetmgr.load_material(f)
-    assetmgr.unload_material(m)
-end
-
-function handler.resource(f)
-    touch_res(assetmgr.resource(f))
-end
-
-function handler.file(f)
-    vfs.realpath(f)
-end
-
-local Extension <const> = {
-    [".prefab"] = "prefab",
-    [".lua"] = "file",
-    [".ecs"] = "file",
-    [".rcss"] = "file",
-    [".rml"] = "file",
-    [".efk"] = "file",
-    [".ttf"] = "file",
-    [".patch"] = "file", --TODO: remove it
-}
-
-local Resource <const> = {
-    [".texture"] = "texture",
-    [".material"] = "material",
-    [".glb"] = "glb",
-}
-
-function handler.dir(f)
-    for file in fs.pairs(fs.path(f)) do
-        local ext = file:extension():string()
-        if fs.is_directory(file) then
-            if Resource[ext] then
-                status_addtask {
-                    type = Resource[ext],
-                    filename = file:string(),
-                }
-            else
+    function handler.dir(f)
+        for file in fs.pairs(fs.path(f)) do
+            if fs.is_directory(file) then
                 status_addtask {
                     type = "dir",
                     filename = file:string(),
                 }
+            else
+                local ext = file:extension():string()
+                if File[ext] then
+                    status_addtask {
+                        type = "file",
+                        filename = file:string(),
+                    }
+                end
             end
-        else
-            if Resource[ext] then
-                status_addtask {
-                    type = Resource[ext],
-                    filename = file:string(),
-                }
-            elseif Extension[ext] then
-                status_addtask {
-                    type = Extension[ext],
-                    filename = file:string(),
-                }
+        end
+    end
+else
+    function handler.file(f)
+    end
+    function handler.compile(f)
+        assetmgr.compile_file(vfs.realpath(f))
+    end
+    function handler.dir(f)
+        for file in fs.pairs(fs.path(f)) do
+            if fs.is_directory(file) then
+                local ext = file:extension():string()
+                if Resource[ext] then
+                    status_addtask {
+                        type = "compile",
+                        filename = file:string(),
+                    }
+                else
+                    status_addtask {
+                        type = "dir",
+                        filename = file:string(),
+                    }
+                end
             end
         end
     end
