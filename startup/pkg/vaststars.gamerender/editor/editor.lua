b77c73b3826860bf66f39dbfb53df8c3388af259
kinbei <kinbei@qq.com>
2023-08-08 14:36:09 +0800

    bugfix

--- startup/pkg/vaststars.gamerender/editor/editor.lua
+++ startup/pkg/vaststars.gamerender/editor/editor.lua
@@ -2,9 +2,7 @@ local ecs = ...
 local world = ecs.world
 
 local objects = require "objects"
-local iprototype = require "gameplay.interface.prototype"
 local iobject = ecs.require "object"
-local iflow_connector = require "gameplay.interface.flow_connector"
 local ROTATORS <const> = require("gameplay.interface.constant").ROTATORS
 
 --
@@ -31,18 +29,6 @@ function M:revert_changes(revert_cache_names)
         end
     end
     iobject.flush() -- object is removed from cache, so we need to flush it, else it will keep the old state
-end
-
-local function refresh_pipe(prototype_name, dir, entry_dir, value)
-    if not iprototype.is_pipe(prototype_name) then
-        return
-    end
-
-    return iflow_connector.set_connection(prototype_name, dir, entry_dir, value)
-end
-
-function M:refresh_pipe(...)
-    return refresh_pipe(...)
 end
 
 return M
