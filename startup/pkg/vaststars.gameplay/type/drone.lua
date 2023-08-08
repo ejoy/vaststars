2c531c2871dd0b42bd6ce81e999802d731e3f516
actboy168 <actboy168@gmail.com>
2023-08-08 11:39:42 +0800

    cleanup

--- startup/pkg/vaststars.gameplay/type/drone.lua
+++ startup/pkg/vaststars.gameplay/type/drone.lua
@@ -7,14 +7,13 @@ function c:ctor(init, pt)
     return {
         drone = {
             prototype = pt.id,
+            home = 0,
             prev = home,
             next = 0,
             mov2 = 0,
-            home = 0,
             maxprogress = 0,
             progress = 0,
             item = 0,
-            home_item = 0,
             status = 0,
         }
     }
