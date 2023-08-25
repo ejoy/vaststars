61dde8f8e89c45985f0b530b645f80a98b349d24
junjie020 <kitchen.gz.020@gmail.com>
2023-08-25 10:37:45 +0800

    fix shader compile error

--- startup/pkg/mod.translucent_plane/assets/tf_fs_func.sh
+++ startup/pkg/mod.translucent_plane/assets/tf_fs_func.sh
@@ -1,6 +1,7 @@
 #include <bgfx_shader.sh>
 #include <bgfx_compute.sh>
 #include <shaderlib.sh>
+#include "common/uvmotion.sh"
 #include "default/inputs_structure.sh"
 
 void CUSTOM_FS_FUNC(in FSInput fsinput, inout FSOutput fsoutput)
