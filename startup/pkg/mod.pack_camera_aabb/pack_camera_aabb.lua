c49bd49363998b16da901c102a347d1a39185e2e
von <1052019421@qq.com>
2023-09-11 16:37:00 +0800

    update pack_camera_aabb

--- startup/pkg/mod.pack_camera_aabb/pack_camera_aabb.lua
+++ startup/pkg/mod.pack_camera_aabb/pack_camera_aabb.lua
@@ -8,44 +8,46 @@ local setting	= import_package "ant.settings"
 local ENABLE_SHADOW<const> = setting:get "graphic/shadow/enable"
 local renderutil= ecs.require "ant.render|util"
 if not ENABLE_SHADOW then
-	renderutil.default_system(pca_sys, "data_changed")
+	renderutil.default_system(pca_sys, "update_camera")
 	return
 end
 
 local INV_Z<const> = true
-local CUSTOM_NPLANE<const>  = math3d.ref(math3d.plane(math3d.vector(0, 5, 0), math3d.vector(0, 1, 0)))
-local CUSTOM_FPLANE<const>  = math3d.ref(math3d.plane(math3d.vector(0, 0, 0), math3d.vector(0, 1, 0)))
+local CUSTOM_NPLANE<const>  = math3d.ref(math3d.vector(0, 1, 0, 5))
+local CUSTOM_FPLANE<const>  = math3d.ref(math3d.vector(0, 1, 0, 0))
 
-function pca_sys:data_changed()
-    for pcae in w:select "pack_camera_aabb:update" do
-        local mq = w:first "main_queue camera_ref:in"
-        local ce <close> = world:entity(mq.camera_ref, "camera_changed?in camera:in scene:in")
-        local main_camera = ce.camera
-        local world_frustum_points = math3d.frustum_points(main_camera.viewprojmat)
-        local keys, fpoints, rays, ipoints = {}, {}, {}, {}
+function pca_sys:update_camera()
+    for pcae in w:select "pack_camera_aabb camera_changed camera:in bounding:update" do
+        local main_camera = pcae.camera
+        local points = math3d.frustum_points(main_camera.viewprojmat)
+        local fp, np
+        local opoints, rays, ipoints = {}, {}, {}
         local function get_frustum_points_rays()
-            if INV_Z then
-                keys = {"lbf", "ltf", "rbf", "rtf", "lbn", "ltn", "rbn", "rtn"}
-            else
-                keys = {"lbn", "ltn", "rbn", "rtn", "lbf", "ltf", "rbf", "rtf"}
+            for i = 1, 4 do
+                fp, np = math3d.array_index(points, i+4), math3d.array_index(points, i)
+                if INV_Z then
+                    rays[#rays+1] = math3d.normalize(math3d.sub(np, fp))
+                    opoints[#opoints+1] = fp
+                else
+                    rays[#rays+1] = math3d.normalize(math3d.sub(fp, np))
+                    opoints[#opoints+1] = np
+                end
             end
-            for i, n in ipairs(keys) do
-                fpoints[n] = math3d.array_index(world_frustum_points, i)
-            end
-            rays.lbn, rays.ltn, rays.rbn, rays.rtn = math3d.sub(fpoints.lbf, fpoints.lbn), math3d.sub(fpoints.ltf, fpoints.ltn), math3d.sub(fpoints.rbf, fpoints.rbn), math3d.sub(fpoints.rtf, fpoints.rtn)
-            rays.lbn, rays.ltn, rays.rbn, rays.rtn = math3d.normalize(rays.lbn), math3d.normalize(rays.ltn), math3d.normalize(rays.rbn),math3d.normalize(rays.rtn)
         end
         local function ray_intersect_with_plane(plane)
             for n, nray in pairs(rays) do
-                local o = fpoints[n]
-                ipoints[#ipoints+1] = math3d.add(math3d.mul(math3d.plane_ray(o, math3d.normalize(nray), plane.v), nray), o)
+                local o = opoints[n]
+                ipoints[#ipoints+1] = math3d.add(math3d.mul(math3d.plane_ray(o, nray, plane.v), nray), o)
             end
         end
         get_frustum_points_rays()
         ray_intersect_with_plane(CUSTOM_NPLANE)
         ray_intersect_with_plane(CUSTOM_FPLANE)
         local aabb_min, aabb_max = math3d.minmax(ipoints)
-        pcae.pack_camera_aabb = math3d.ref(math3d.aabb(aabb_min, aabb_max))          
-    end
+        local update_aabb        = math3d.marked_aabb(aabb_min, aabb_max)
+        math3d.unmark(pcae.bounding.scene_aabb)
+        pcae.bounding.scene_aabb = update_aabb
+        pcae.bounding.aabb       = update_aabb     
+    end 
 end
 
