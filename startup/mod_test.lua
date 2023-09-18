df834917073a7df2279c08e79c33d5069feaa332
actboy168 <actboy168@gmail.com>
2023-09-18 17:13:55 +0800

    update

--- startup/mod_test.lua
+++ startup/mod_test.lua
@@ -1,23 +1,7 @@
 package.path = "engine/?.lua"
 require "bootstrap"
 import_package "ant.window".start {
-    import = {
-        "@vaststars.mod.test",
-    },
-    pipeline = {
-        "init",
-        "update",
-        "exit",
-    },
-    system = {
-        "vaststars.mod.test|init_system",
-    },
-    interface = {
-        "ant.objcontroller|iobj_motion",
-    },
-    policy = {
-        "ant.scene|scene_object",
-        "ant.render|render",
-        "ant.render|render_queue",
+    feature = {
+        "vaststars.mod.test",
     }
 }
