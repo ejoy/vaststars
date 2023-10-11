d386479115fbb8026ac2495bb09b1bbaae5212ee
Cloud Wu <cloudwu@gmail.com>
2023-10-11 13:06:09 +0800

    mark large textures

--- startup/pkg/vaststars.webcgi/texture.lua
+++ startup/pkg/vaststars.webcgi/texture.lua
@@ -15,7 +15,7 @@ local html_footer = [[
 </body>
 ]]
 
-local KEYS = { "id", "name", "type", "info", "flag", "handle" }
+local KEYS = { "id", "info", "name", "type", "handle" }
 
 local function default_info(v)
 	return v
@@ -27,10 +27,44 @@ function INFO.id(v)
 	return ('<a href="texture/%d">%d</a>'):format(v,v)
 end
 
+local ignore_info = {
+	storageSize = true,
+	width = true,
+	height = true,
+	numLayers = true,
+	depth = true,
+	cubeMap = true,
+	numMips = true,
+	format = true,
+	bitsPerPixel = true,
+}
+
 function INFO.info(v)
 	local info = {}
+	local size = v.storageSize
+	if size > 65536 then
+		table.insert(info, ('<div style="color:red">%s</div>'):format(size))
+	else
+		table.insert(info, tostring(size))
+	end
+	table.insert(info, ("%dx%dx%d"):format(v.width, v.height, v.bitsPerPixel))
+	if v.numLayers ~= 1 then
+		table.insert(info, "layer:".. v.numLayers)
+	end
+	if v.depth ~= 1 then
+		table.insert(info, "depth:" .. v.depth)
+	end
+	if v.cubeMap then
+		table.insert(info, "cubemap")
+	end
+	if v.numMips ~= 1 then
+		table.insert(info, "mips:" .. v.numMips)
+	end
+	table.insert(info, v.format)
 	for k,v in pairs(v) do
-		table.insert(info, k .. ":" .. tostring(v))
+		if not ignore_info[k] then
+			table.insert(info, k .. ":" .. tostring(v))
+		end
 	end
 	return table.concat(info, ",")
 end
