94b321e516d1458e98210b32391c011a68e27cfa
kinbei <kinbei@qq.com>
2023-10-16 14:54:18 +0800

    cleanup

--- startup/pkg/vaststars.gamerender/gameplay/prototype_cache/recipe_unlocked.lua
+++ startup/pkg/vaststars.gamerender/gameplay/prototype_cache/recipe_unlocked.lua
@@ -1,13 +1,6 @@
 local iprototype = require "gameplay.interface.prototype"
 
 return function ()
-    local function length(t)
-        local n = 0
-        for _ in pairs(t) do
-            n = n + 1
-        end
-        return n
-    end
     local mt = {}
     function mt:__index(k)
         local v = {}
@@ -27,11 +20,6 @@ return function ()
             for _, recipe in ipairs(typeobject.effects.unlock_recipe) do
                 recipe_tech[recipe][typeobject.name] = true
             end
-        end
-    end
-    for recipe, v in pairs(recipe_tech) do
-        if length(v) > 1 then
-            error(("recipe `%s` is unlocked by multiple techs: %s"):format(recipe, table.concat(v, ", ")))
         end
     end
 
