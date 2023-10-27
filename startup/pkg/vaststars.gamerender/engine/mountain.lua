c3b281729889250019ff46f77e4abb6d94d1c3d1
junjie020 <kitchen.gz.020@gmail.com>
2023-10-27 09:49:01 +0800

    stone mountain use base 0 index

--- startup/pkg/vaststars.gamerender/engine/mountain.lua
+++ startup/pkg/vaststars.gamerender/engine/mountain.lua
@@ -15,13 +15,11 @@ local MOUNTAIN_MASKS
 local function set_masks(masks, r, v)
     -- x, y base 0
     local x0, y0, ww, hh = r[1], r[2], r[3], r[4]
-    --base 1
-    local x1, y1 = x0+1, y0+1
 
     --range:[x1, x1+ww), [y1, y+hh)
-    for x=x1, x1+ww-1 do
-        for y=y1, y1+hh-1 do
-            masks[icoord:coord2idx1(x, y, WIDTH)] = v
+    for x=x0, x0+ww-1 do
+        for y=y0, y0+hh-1 do
+            masks[icoord.coord2idx(x, y)] = v
         end
     end
 end
@@ -41,14 +39,14 @@ end
 
 local function merge_indices(indices, width, height, range)
     local m = {}
-    for iz=1, height, range do
-        for ix=1, width, range do
-            local idx = (iz-1)*width+ix
+    for iz=0, height-1, range do
+        for ix=0, width-1, range do
+            local idx = iz*width+ix
 
             local function is_sub_range(baseidx, range)
-                for izz=1, range do
-                    for ixx=1, range do
-                        local sidx = baseidx + (izz-1) * width + ixx
+                for izz=0, range-1 do
+                    for ixx=0, range-1 do
+                        local sidx = baseidx + izz * width + ixx
                         if indices[sidx] == 0 then
                             return false
                         end
@@ -71,12 +69,12 @@ local function build_sub_indices(masks)
         local m = merge_indices(masks, WIDTH, HEIGHT, range)
         for _, info in ipairs(m) do
             subindices[info.baseidx] = info
-            local x, y = icoord.idx2coord(info.baseidx-1, WIDTH)
-            set_masks(masks, {x-1, y-1, info.sidx, info.sidx}, 1)
+            local x, y = icoord.idx2coord(info.baseidx)
+            set_masks(masks, {x, y, info.sidx, info.sidx}, 1)
         end
     end
 
-    for idx, mask in ipairs(masks) do
+    for idx, mask in pairs(masks) do
         if 0 ~= mask then
             subindices[idx] = {sidx=1, baseidx=idx}
         end
@@ -85,8 +83,8 @@ local function build_sub_indices(masks)
 end
 
 local function idxoffset(baseidx, sidx)
-    local subidx = (sidx // 2)+1
-    return baseidx + (subidx-1)*WIDTH+subidx
+    local subidx = sidx // 2
+    return baseidx + subidx*WIDTH+subidx
 end
 
 local M = {}
@@ -101,18 +99,17 @@ function M:create()
         local sidx = info.sidx
         local idx = idxoffset(baseidx, sidx)
 
-        local x, y = icoord.idx2coord1(idx, WIDTH)
-        assert(1<=x and x<=WIDTH and 1<=y and y<=HEIGHT)
-        local x0, y0 = x-1, y-1
+        local x0, y0 = icoord.idx2coord(idx)
+        assert(0<=x0 and x0<WIDTH and 0<=y0 and y0<HEIGHT)
         local indices = groups[igroup.id(x0, y0)]
-        indices[#indices+1] = {coord = {x, y}, pos = icoord.lefttop_position(x0, y0), sidx=sidx}
+        indices[#indices+1] = {coord = {x0, y0}, pos = icoord.lefttop_position(x0, y0), sidx=sidx}
     end
 
     im.create(groups, WIDTH, HEIGHT)
 end
 
 function M:has_mountain(x, y)
-    local idx = icoord:coord2idx1(x, y, WIDTH)
+    local idx = icoord.coord2idx(x, y)
     return (0 ~= assert(MOUNTAIN_MASKS[idx], ("Invalid x:%d, y:%d, idx"):format(x, y, idx)))
 end
 
