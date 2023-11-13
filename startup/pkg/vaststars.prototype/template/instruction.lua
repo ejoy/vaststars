773b984ce66ab74c009b61394389270af23e74fc
uglyfrog78 <rendong.ren@alibaba-inc.com>
2023-11-13 15:16:36 +0800

    update

--- startup/pkg/vaststars.prototype/template/instruction.lua
+++ startup/pkg/vaststars.prototype/template/instruction.lua
@@ -57,7 +57,7 @@ local entities = { {
   y = 93
 }, {
   dir = "N",
-  items = { { "机械科技包", 26 }, { "化学科技包", 26 }, { "物理科技包", 26 }, { "地质科技包", 26 } },
+  items = { { "机械科技包", 22 }, { "化学科技包", 22 }, { "物理科技包", 22 }, { "地质科技包", 22 } },
   prototype_name = "仓库I",
   x = 127,
   y = 89
@@ -133,14 +133,135 @@ local entities = { {
   prototype_name = "无人机平台I",
   x = 121,
   y = 132
-} }
-local backpack = {}
-local road = { {
-  dir = "E",
-  prototype_name = "砖石公路-U型",
-  x = 154,
-  y = 82
 }, {
+  dir = "S",
+  prototype_name = "停车站",
+  x = 176,
+  y = 84
+}, {
+  dir = "S",
+  items = { { "supply", "碎石", 1 }, { "supply", "铁矿石", 1 }, { "supply", "铝矿石", 1 }, { "demand", "地质科技包", 3 } },
+  prototype_name = "物流站",
+  x = 152,
+  y = 84
+}, {
+  dir = "W",
+  items = { { "demand", "铁矿石", 3 }, { "demand", "碎石", 2 }, { "supply", "地质科技包", 2 }, { "demand", "铝矿石", 1 } },
+  prototype_name = "物流站",
+  x = 172,
+  y = 90
+}, {
+  dir = "N",
+  prototype_name = "组装机I",
+  recipe = "地质科技包1",
+  x = 167,
+  y = 92
+}, {
+  dir = "N",
+  prototype_name = "无人机平台I",
+  x = 171,
+  y = 91
+}, {
+  dir = "N",
+  prototype_name = "无人机平台I",
+  x = 171,
+  y = 92
+}, {
+  dir = "N",
+  items = {},
+  prototype_name = "仓库I",
+  x = 153,
+  y = 86
+}, {
+  dir = "N",
+  items = {},
+  prototype_name = "仓库I",
+  x = 154,
+  y = 86
+}, {
+  dir = "N",
+  prototype_name = "无人机平台I",
+  x = 151,
+  y = 87
+}, {
+  dir = "N",
+  prototype_name = "无人机平台I",
+  x = 156,
+  y = 87
+}, {
+  dir = "N",
+  prototype_name = "无人机平台I",
+  x = 153,
+  y = 87
+}, {
+  dir = "N",
+  prototype_name = "无人机平台I",
+  x = 154,
+  y = 87
+}, {
+  dir = "N",
+  prototype_name = "采矿机I",
+  recipe = "碎石挖掘",
+  x = 149,
+  y = 89
+}, {
+  dir = "N",
+  prototype_name = "采矿机I",
+  recipe = "铁矿石挖掘",
+  x = 153,
+  y = 89
+}, {
+  dir = "N",
+  prototype_name = "采矿机I",
+  recipe = "铝矿挖掘",
+  x = 157,
+  y = 89
+}, {
+  dir = "N",
+  prototype_name = "科研中心I",
+  x = 147,
+  y = 85
+}, {
+  dir = "N",
+  prototype_name = "科研中心I",
+  x = 158,
+  y = 85
+}, {
+  dir = "N",
+  prototype_name = "组装机I",
+  recipe = "地质科技包1",
+  x = 167,
+  y = 89
+}, {
+  dir = "N",
+  prototype_name = "组装机I",
+  recipe = "地质科技包1",
+  x = 170,
+  y = 94
+}, {
+  dir = "N",
+  prototype_name = "组装机I",
+  recipe = "地质科技包1",
+  x = 170,
+  y = 87
+}, {
+  dir = "N",
+  items = { { "地质科技包", 0 } },
+  prototype_name = "仓库I",
+  x = 170,
+  y = 91
+}, {
+  dir = "N",
+  items = { { "地质科技包", 0 } },
+  prototype_name = "仓库I",
+  x = 170,
+  y = 92
+} }
+local backpack = { {
+  count = 1,
+  prototype_name = "熔炼炉I"
+} }
+local road = { {
   dir = "E",
   prototype_name = "砖石公路-I型",
   x = 156,
@@ -260,14 +381,33 @@ local road = { {
   prototype_name = "砖石公路-U型",
   x = 182,
   y = 82
+}, {
+  dir = "E",
+  prototype_name = "砖石公路-I型",
+  x = 154,
+  y = 82
+}, {
+  dir = "E",
+  prototype_name = "砖石公路-I型",
+  x = 152,
+  y = 82
+}, {
+  dir = "E",
+  prototype_name = "砖石公路-U型",
+  x = 150,
+  y = 82
 } }
 local mineral = {
 ["115,129"] = "碎石",
 ["120,129"] = "铁矿石",
 ["125,129"] = "地热气",
 ["130,129"] = "铝矿石",
-["135,129"] = "砂岩"
+["135,129"] = "砂岩",
+["149,89"] = "碎石",
+["153,89"] = "铁矿石",
+["157,89"] = "铝矿石"
 }
+
 return {
   name = "游戏教程",
   entities = entities,
