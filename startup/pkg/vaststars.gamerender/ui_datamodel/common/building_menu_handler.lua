0b1406b98aa7dd252679da3d08aa3b773f882d62
kinbei <kinbei@qq.com>
2023-11-23 14:16:20 +0800

    cleanup

---
+++ startup/pkg/vaststars.gamerender/ui_datamodel/common/building_menu_handler.lua
@@ -0,0 +1,108 @@
+local ecs = ...
+local world = ecs.world
+local w = world.w
+
+local ICONS <const> = ecs.require "vaststars.prototype|building_menu.icons"
+local CUSTOM_COMMANDS <const> = ecs.require "vaststars.prototype|building_menu.custom_commands"
+local AUDIOS <const> = ecs.require "vaststars.prototype|building_menu.audios"
+
+local TRANSFORM_DELTA <const> = 18
+local BUILDING_MENU_POSITIONS = {}
+for i = 1, 5 do
+    BUILDING_MENU_POSITIONS[i] = {
+        outer_transform = ("rotate(%sdeg)"):format(-TRANSFORM_DELTA * (i - 1)),
+        inner_transform = ("rotate(%sdeg)"):format(TRANSFORM_DELTA * (i - 1)),
+    }
+end
+
+local DEFAULT_OFFSETS = {
+    [1] = {
+        [1] = BUILDING_MENU_POSITIONS[3],
+    },
+    [2] = {
+        [1] = BUILDING_MENU_POSITIONS[2],
+        [2] = BUILDING_MENU_POSITIONS[4],
+    },
+    [3] = {
+        [1] = BUILDING_MENU_POSITIONS[2],
+        [2] = BUILDING_MENU_POSITIONS[3],
+        [3] = BUILDING_MENU_POSITIONS[4],
+    },
+    [4] = {
+        [1] = BUILDING_MENU_POSITIONS[2],
+        [2] = BUILDING_MENU_POSITIONS[3],
+        [3] = BUILDING_MENU_POSITIONS[4],
+        [4] = BUILDING_MENU_POSITIONS[5],
+    },
+    [5] = {
+        [1] = BUILDING_MENU_POSITIONS[1],
+        [2] = BUILDING_MENU_POSITIONS[2],
+        [3] = BUILDING_MENU_POSITIONS[3],
+        [4] = BUILDING_MENU_POSITIONS[4],
+        [5] = BUILDING_MENU_POSITIONS[5],
+    }
+}
+
+local COMMANDS = {
+    "set_transfer_source",
+    "transfer_source",
+    "remove_lorry",
+    "move",
+    "lorry_factory_inc_lorry",
+    "set_item",
+    "set_recipe",
+    "copy",
+    "inventory",
+    "transfer",
+    "teardown",
+}
+
+local COMMAND_HANDLERS <const> = {
+    ["transfer"] = function(status, button)
+        button.number = status.transfer_count
+        return button
+    end,
+    ["transfer_source"] = function(status, button)
+        button.selected = true
+        return button
+    end,
+}
+
+local function create_button(command)
+    return {
+        command = command,
+        background_image = ICONS[command],
+        number = "", -- can have a value of either a digit or '+', '' 
+        selected = false,
+        audio = AUDIOS[command],
+    }
+end
+
+local function set_button_offset(buttons)
+    -- console.log("#buttons", #buttons)
+    if #buttons > 0 then
+        local offsets = DEFAULT_OFFSETS[#buttons] or error(("(%s)"):format(#buttons))
+        for i = 1, #buttons do
+            for k, v in pairs(offsets[i]) do
+                buttons[i][k] = v
+            end
+        end
+    end
+end
+
+return function(prototype_name, status)
+    local buttons = {}
+    for _, command in ipairs(CUSTOM_COMMANDS[prototype_name] or COMMANDS) do
+        if status[command] then
+            local button = create_button(command)
+            local h = COMMAND_HANDLERS[command]
+            if h then
+                button = h(status, button)
+            end
+            buttons[#buttons + 1] = button
+        end
+    end
+    set_button_offset(buttons)
+
+    return buttons
+end
---
+++ startup/pkg/vaststars.prototype/building_menu/custom_commands.lua
@@ -0,0 +1,67 @@
+return {
+    -- 采矿建筑仅保留获取
+    ["采矿机I"] = {
+        "set_transfer_source",
+        "transfer_source",
+        "teardown",
+    },
+    ["采矿机II"] = {
+        "set_transfer_source",
+        "transfer_source",
+        "teardown",
+    },
+    ["采矿机III"] = {
+        "set_transfer_source",
+        "transfer_source",
+        "teardown",
+    },
+
+    -- 废墟建筑仅保留获取
+    ["机身残骸"] = {
+        "set_transfer_source",
+        "transfer_source",
+        "teardown",
+    },
+    ["机翼残骸"] = {
+        "set_transfer_source",
+        "transfer_source",
+        "teardown",
+    },
+    ["机头残骸"] = {
+        "set_transfer_source",
+        "transfer_source",
+        "teardown",
+    },
+    ["机尾残骸"] = {
+        "set_transfer_source",
+        "transfer_source",
+        "teardown",
+    },
+
+    -- 液体输入/输出建筑仅保留移动和复制
+    ["空气过滤器I"] = {
+        "move",
+        "copy",
+        "teardown",
+    },
+    ["空气过滤器II"] = {
+        "move",
+        "copy",
+        "teardown",
+    },
+    ["空气过滤器III"] = {
+        "move",
+        "copy",
+        "teardown",
+    },
+    ["地下水挖掘机I"] = {
+        "move",
+        "copy",
+        "teardown",
+    },
+    ["地下水挖掘机II"] = {
+        "move",
+        "copy",
+        "teardown",
+    },
+}
