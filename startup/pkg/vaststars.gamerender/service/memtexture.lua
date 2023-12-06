ef8ee483a9dfa0c9e650a91834ede103e672535f
von <1052019421@qq.com>
2023-12-06 16:51:13 +0800

    update memtexture

--- startup/pkg/vaststars.gamerender/service/memtexture.lua
+++ startup/pkg/vaststars.gamerender/service/memtexture.lua
@@ -2,60 +2,15 @@ local ltask 		= require "ltask"
 local ServiceWorld  = ltask.queryservice "ant.window|world"
 local S 			= {}
 
-local DEFAULT_SIZE_CONFIGS <const> = {
-	{
-		width  = 512,
-		height = 512,
-	},
-	{
-		width  = 256,
-		height = 256,
-	},
-	{
-		width  = 128,
-		height = 128,
-	},	
-}
-
-local DEFAULT_STATIC_ROT_CONFIGS <const> = {
-	{0.785, 0, 0},
-	{0.785, -0.785, 0},
-	{0.785, 0.785, 0},
-}
-
-local DEFAULT_DYNAMIC_ROT_CONFIG = {0.785, 0, 0}
-
-local RT_CACHE = {}
-
 local function init()
 	local textmgr = ltask.uniqueservice "ant.resource_manager|resource"
 	ltask.call(textmgr, "register", "mem", ltask.self())
 end
 
 function S.load(path, config)
+	local type, size, rot, dis = ltask.call(ServiceWorld, "parse_prefab_config", config)
 
-	local function parse_config()
-		return config:match "%w+:(%a),(%d),(%d),?([%d%.]*)"
-	end
-
-	local function get_rotation(rot_config, is_dynamic)
-		local rot = rot_config and DEFAULT_STATIC_ROT_CONFIGS[tonumber(rot_config)]
-		if not rot then
-			if is_dynamic then
-				rot = DEFAULT_STATIC_ROT_CONFIGS[1]
-			else
-				rot = DEFAULT_DYNAMIC_ROT_CONFIG
-			end
-		end
-		return rot
-	end
-
-	local type, size_config, rot_config, dis_config = parse_config()
-	local is_dynamic = type == 'd'
-	local rot 		 = get_rotation(rot_config, is_dynamic)
-	local size 		 = size_config and DEFAULT_SIZE_CONFIGS[tonumber(size_config)] or DEFAULT_SIZE_CONFIGS[1]
-	local dis 		 = dis_config and tonumber(dis_config) or 1.0
-	local c 		 = {
+	local c = {
 		info = {
             width = size.width,
             height = size.height,
@@ -72,15 +27,13 @@ function S.load(path, config)
 		handle = nil,
 	}
 
-	local rt_idx
-	c.handle, rt_idx = ltask.call(ServiceWorld, "create_mem_texture_prefab", path, size.width, size.height, rot, dis, is_dynamic)
-	RT_CACHE[c.handle] = rt_idx
-
-	return c, is_dynamic
+	c.handle = ltask.call(ServiceWorld, "get_portrait_handle", size.width, size.height, type)
+	ltask.call(ServiceWorld, "set_portrait_prefab", path, rot, dis, type)
+	return c, true
 end
 
 function S.unload(handle)
-	ltask.call(ServiceWorld, "destroy_mem_texture_prefab", RT_CACHE[handle])
+
 end
 
 init()
