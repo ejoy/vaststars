2a54ff0fa2e00a8362a26d3bbccd2528f66e7dad
uglyfrog78 <rendong.ren@alibaba-inc.com>
2023-12-11 16:45:30 +0800

    update

--- startup/pkg/vaststars.prototype/technology.lua
+++ startup/pkg/vaststars.prototype/technology.lua
@@ -1808,7 +1808,7 @@ prototype "铝生产" {
   type = { "tech" },
   effects = {
     unlock_recipe = {"碾碎铝矿石","铝矿石浮选","氧化铝","铝板1"},
-    unlock_item = {"铝板","碾碎铝矿石","氧化铝"},
+    unlock_item = {"铝板","碾碎铝矿石","碾碎铁矿石","氧化铝"},
   },
   prerequisites = {"无机化学","冶金学2"},
   ingredients = {
