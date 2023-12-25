a2f64f0414ce5489bc57bbedab8fec2aff2571cf
actboy168 <actboy168@gmail.com>
2023-12-25 14:21:00 +0800

    disable window_size

--- startup/main.lua
+++ startup/main.lua
@@ -2,7 +2,8 @@ package.path = "/engine/?.lua"
 require "bootstrap"
 
 import_package "ant.window".start {
-    window_size = "1280x720",	-- todo: read user config
+    -- todo: read user config
+    -- window_size = "1280x720",
     feature = {
         "vaststars.gamerender|login",
     }
