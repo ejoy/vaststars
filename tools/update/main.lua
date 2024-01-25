e967202460402c711ef7be141d2f81c8e20bf6ec
actboy168 <actboy168@gmail.com>
2024-01-25 16:53:16 +0800

    update tools

--- tools/update/main.lua
+++ tools/update/main.lua
@@ -1,19 +1,7 @@
 package.path = "/engine/?.lua"
 require "bootstrap"
 
-local function start_ltask()
-    local task = dofile "/engine/task/bootstrap.lua"
-    local exclusive = { "timer", "subprocess" }
-	local directory = require "directory"
-	local log_path = directory.app_path()
-    task {
-        bootstrap = { "vaststars.update|boot" },
-        logger = { "logger" },
-        exclusive = exclusive,
-        debuglog = (log_path / "debug.log"):string(),
-        crashlog = (log_path / "crash.log"):string(),
-        worker = 4,
-    }
-end
-
-start_ltask()
+dofile "/engine/ltask.lua" {
+    bootstrap = { "vaststars.update|boot" },
+    exclusive = { "timer", "subprocess" },
+}
