9138fe7bebf62e27735fdb085c4499ebfd4af307
Cloud Wu <cloudwu@gmail.com>
2024-02-21 15:58:06 +0800

    -f window_size

--- startup/main.lua
+++ startup/main.lua
@@ -3,13 +3,19 @@ require "bootstrap"
 
 local options = {}
 
-if arg[1] == "-f" then
-	options.window_size = "1280x720"
+for i, a in ipairs(arg) do
+	if a == "-f" then
+		local size = "1280x720"
+		if arg[i+1] and arg[i+1]:sub(1,1) ~= "-" then
+			size = arg[i+1]
+		end
+		options.window_size = size
+	end
 end
 
 import_package "ant.window".start {
-    window_size = options.window_size,
-    feature = {
-        "vaststars.gamerender|login",
-    }
+	window_size = options.window_size,
+	feature = {
+		"vaststars.gamerender|login",
+	}
 }
