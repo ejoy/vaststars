5698b8e18a7be54e4722c117e0fa57d6cbb02de1
kinbei <kinbei@qq.com>
2024-02-26 17:32:46 +0800

    cleanup

--- startup/pkg/vaststars.gamerender/engine/quad_lines_entity.lua
+++ startup/pkg/vaststars.gamerender/engine/quad_lines_entity.lua
@@ -20,31 +20,20 @@ local iom = ecs.require "ant.objcontroller|obj_motion"
 
 local M = {}
 function M.create(position, quad_num, dir)
-    local ids = {}
-    local eid = ientity.create_quad_lines_entity({}, MATERIAL, 10, 10.0, false, "translucent")
-
-    ids[#ids+1] = eid
-    ids[#ids+1] = world:create_entity {
-        policy = {},
-        data = {
-            on_ready = function ()
-                local e <close> = world:entity(eid, "render_object:update")
-                local ro = e.render_object
-                ro.ib_start, ro.ib_num = 0, 0 -- *6
-                ro.vb_start, ro.vb_num = 0, 0 -- *4
-                ro.ib_num = quad_num * 6
-                ro.vb_num = quad_num * 4
-                iom.set_position(e, math3d.add(position, DELTA_VEC[dir]))
-                iom.set_rotation(e, ROTATORS[dir])
-            end
-        }
-    }
+    local eid = ientity.create_quad_lines_entity({}, MATERIAL, 10, 10.0, false, "translucent", function(e)
+        w:extend(e, "render_object:update")
+        local ro = e.render_object
+        ro.ib_start, ro.ib_num = 0, 0 -- *6
+        ro.vb_start, ro.vb_num = 0, 0 -- *4
+        ro.ib_num = quad_num * 6
+        ro.vb_num = quad_num * 4
+        iom.set_position(e, math3d.add(position, DELTA_VEC[dir]))
+        iom.set_rotation(e, ROTATORS[dir])
+    end)
 
     return {
         remove = function()
-            for _, id in ipairs(ids) do
-                world:remove_entity(id)
-            end
+            world:remove_entity(eid)
         end
     }
 end
