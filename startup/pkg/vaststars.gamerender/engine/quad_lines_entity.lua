0bfe94ed963d00c2a404f1ea813ab6d12404615a
junjie020 <kitchen.gz.020@gmail.com>
2024-02-27 09:06:33 +0800

    use new quad entity

--- startup/pkg/vaststars.gamerender/engine/quad_lines_entity.lua
+++ startup/pkg/vaststars.gamerender/engine/quad_lines_entity.lua
@@ -1,7 +1,6 @@
 
 local ecs = ...
 local world = ecs.world
-local w = world.w
 
 local CONSTANT = require "gameplay.interface.constant"
 local ROTATORS = CONSTANT.ROTATORS
@@ -16,23 +15,14 @@ local DELTA_VEC <const> = {
 }
 
 local ientity = ecs.require "ant.entity|entity"
-local iom = ecs.require "ant.objcontroller|obj_motion"
 
 local M = {}
 function M.create(position, quad_num, dir)
-    local eid = ientity.create_quad_lines_entity({}, MATERIAL, 10, 10.0, false, "translucent", function(e)
-        w:extend(e, "render_object:update")
-        local ro = e.render_object
-        ro.ib_start, ro.ib_num = 0, 0 -- *6
-        ro.vb_start, ro.vb_num = 0, 0 -- *4
-        ro.ib_num = quad_num * 6
-        ro.vb_num = quad_num * 4
-        iom.set_position(e, math3d.add(position, DELTA_VEC[dir]))
-        iom.set_rotation(e, ROTATORS[dir])
-    end)
-
+    local UNIT<const> = 10.0
+    local ww = UNIT
+    local eid = ientity.create_quad_entity(MATERIAL, {t=math3d.add(position, DELTA_VEC[dir]), r=ROTATORS[dir]}, {x=-0.5*ww, y=0, w=ww, h=UNIT*quad_num}, {x=0, y=0, w=1.0, h=quad_num})
     return {
-        remove = function()
+        remove = function ()
             world:remove_entity(eid)
         end
     }
