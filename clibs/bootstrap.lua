dfb7190d9e1ef6d01e7d539c4ab55e83961cc091
actboy168 <actboy168@gmail.com>
2024-03-05 17:14:01 +0800

    fixes bug

--- clibs/bootstrap.lua
+++ clibs/bootstrap.lua
@@ -36,7 +36,8 @@ elseif arg[0] == "-p" then
     arg[0] = "3rd/ant/tools/filepack/main.lua"
     table.insert(arg, 1, "../../startup")
 elseif arg[0] == "-d" then
-    arg[0] = "3rd/ant/tools/editor/launch/main.lua"
+    arg[0] = "3rd/ant/tools/editor/main.lua"
+    table.insert(arg, 1, "../../startup")
 elseif arg[0] == nil or arg[0] == "" then
     arg[0] = "startup/main.lua"
 elseif arg[0]:sub(1,1) == "-" then
