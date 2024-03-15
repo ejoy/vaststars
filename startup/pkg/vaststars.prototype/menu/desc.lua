6c016586c43c4bd14f32cd5113026620ac62d816
uglyfrog <uglyfrog78@163.com>
2024-03-15 15:46:01 +0800

    desc of buttons modified

--- startup/pkg/vaststars.prototype/menu/desc.lua
+++ startup/pkg/vaststars.prototype/menu/desc.lua
@@ -10,8 +10,8 @@ return {
 
     ["bulk_opt.reset"] = "清空所有标记建筑",
     ["bulk_opt.operate"] = "移动或者删除建筑",
-    ["bulk_opt.unselect"] = "选择框中的被标记建筑取消标记状态",
-    ["bulk_opt.select"] = "标记选择框中的建筑",
+    ["bulk_opt.unselect"] = "选框中被标记建筑取消标记状态",
+    ["bulk_opt.select"] = "标记选框中的建筑",
     ["bulk_opt.remove"] = "删除所有标记的建筑",
     ["bulk_opt.remove_confirm"] = "确认删除所有标记的建筑",
     ["bulk_opt.move"] = "移动所有标记的建筑",
