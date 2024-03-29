feebbfc3a85b520ab5e947d758fdf3d69fd0be86
actboy168 <actboy168@gmail.com>
2024-03-29 16:55:20 +0800

    update

--- startup/pkg/vaststars.gameupdate/service/download.lua
+++ startup/pkg/vaststars.gameupdate/service/download.lua
@@ -1,6 +1,7 @@
 
 import_package "ant.hwi".init_bgfx()
 local version = import_package "vaststars.version"
+local engine = import_package "ant.engine"
 local platform = require "bee.platform"
 local fs = require "bee.filesystem"
 local bgfx = require "bgfx"
@@ -29,29 +30,7 @@ local function unzip(zippath, output)
     f:close()
 end
 
-local function app_path(name)
-	if platform.os == "windows" then
-		return fs.path(os.getenv "LOCALAPPDATA") / name
-	elseif platform.os == "linux" then
-		return fs.path(os.getenv "XDG_DATA_HOME" or (os.getenv "HOME" .. "/.local/share")) / name
-	elseif platform.os == "macos" then
-		return fs.path(os.getenv "HOME" .. "/Library/Caches") / name
-	else
-		error "unknown os"
-	end
-end
-
-local sandbox_path = (function ()
-	if platform.os == "ios" then
-		local ios = require "ios"
-		return fs.path(ios.directory(ios.NSDocumentDirectory))
-	elseif platform.os == "android" then
-		local android = require "android"
-		return fs.path(android.directory(android.ExternalDataPath))
-	else
-		return app_path "ant" / "sandbox"
-	end
-end)()
+local sandbox_path = engine.app_path()
 
 fs.create_directories(sandbox_path / "download")
 
