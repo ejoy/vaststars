477f0e7201a891b40d4c9157f3c5483988aab32e
actboy168 <actboy168@gmail.com>
2024-05-08 21:05:13 +0800

    simplify

--- clibs/bootstrap.lua
+++ clibs/bootstrap.lua
@@ -1,54 +1,16 @@
-local arguments = {}
-local options = {}
-do
-    local extra <const> = {
-        ["-e"] = true,
-    }
-    local i = 1
-    while true do
-        if arg[i] == nil then
-            break
-        elseif arg[i]:sub(1, 1) == "-" then
-            if extra[arg[i]] then
-                options[arg[i]] = arg[i+1]
-                i = i + 1
-            else
-                options[arg[i]] = true
-            end
-        else
-            arguments[#arguments+1] = arg[i]
-        end
-        i = i + 1
-    end
-end
-
-arg = {}
-
-if options["-s"] then
+if arg[1] == "-s" then
     arg[0] = "3rd/ant/tools/fileserver/main.lua"
     arg[1] = "../../startup"
-elseif options["-p"] then
+elseif arg[1] == "-p" then
     arg[0] = "3rd/ant/tools/filepack/main.lua"
     arg[1] = "../../startup"
-elseif options["-d"] then
+elseif arg[1] == "-d" then
     arg[0] = "3rd/ant/tools/editor/main.lua"
     arg[1] = "../../startup"
-elseif arguments[1] == nil or arguments[1] == "" then
+elseif arg[1] == nil or arg[1] == "" then
     arg[0] = "startup/main.lua"
 else
-    arg[0] = table.remove(arguments, 1)
-end
-
-table.move(arguments, 1, #arguments, #arg+1, arg)
-
-options["-s"] = nil
-options["-p"] = nil
-options["-d"] = nil
-for k, v in pairs(options) do
-    arg[#arg+1] = k
-    if v ~= true then
-        arg[#arg+1] = v
-    end
+    arg[0] = table.remove(arg, 1)
 end
 
 local fs = require "bee.filesystem"
